4575803bde77d4fc7c6b1f762eceddc9ff79f8a5
Craig Fowler <craig@csf-dev.com>
2017-04-08 21:57:46 +0100

    Add service which gets the current user information when required

---
+++ Agiil.Services/Auth/CurrentUserReader.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Agiil.Domain.Auth;
+using CSF.Data;
+using CSF.Data.Entities;
+
+namespace Agiil.Services.Auth
+{
+  public class CurrentUserReader : ICurrentUserReader
+  {
+    readonly IIdentityReader identityReader;
+    readonly IQuery query;
+
+    public User GetCurrentUser()
+    {
+      var userInfo = GetUserInfo();
+      if(userInfo == null)
+      {
+        return null;
+      }
+      return GetUser(userInfo);
+    }
+
+    public User RequireCurrentUser()
+    {
+      var userInfo = GetUserInfo();
+      if(userInfo == null)
+      {
+        throw new CurrentUserNotIdentifiedException(Resources.ExceptionMessages.UserMustBeLoggedIn);
+      }
+      var user = GetUser(userInfo);
+      if(user == null)
+      {
+        throw new CurrentUserNotIdentifiedException(Resources.ExceptionMessages.UserMustExist);
+      }
+      return user;
+    }
+
+    protected ICurrentUserInfo GetUserInfo()
+    {
+      return identityReader.GetCurrentUserInfo();
+    }
+
+    protected User GetUser(ICurrentUserInfo userInfo)
+    {
+      return query.Get(userInfo.Identity);
+    }
+
+    public CurrentUserReader(IIdentityReader identityReader, IQuery query)
+    {
+      if(query == null)
+        throw new ArgumentNullException(nameof(query));
+      if(identityReader == null)
+        throw new ArgumentNullException(nameof(identityReader));
+      
+      this.query = query;
+      this.identityReader = identityReader;
+    }
+  }
+}
---
+++ Agiil.Services/Auth/ICurrentUserReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Agiil.Domain.Auth;
+
+namespace Agiil.Services.Auth
+{
+  public interface ICurrentUserReader
+  {
+    User RequireCurrentUser();
+
+    User GetCurrentUser();
+  }
+}
