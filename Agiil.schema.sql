f7d117812c4b19607e0e7ddde134b3ed9a920e8d
Craig Fowler <craig@csf-dev.com>
2017-04-08 20:34:19 +0100

    Add new empty test project for Services assembly

--- Agiil.schema.sql
+++ Agiil.schema.sql
@@ -3,11 +3,20 @@
 
     drop table if exists User
 
+    drop table if exists Ticket
+
     PRAGMA foreign_keys = ON
 
     create table User (
         id BIGINT not null,
        Username TEXT,
        SerializedCredentials TEXT,
+       primary key (id)
+    )
+
+    create table Ticket (
+        id BIGINT not null,
+       Title TEXT,
+       Description TEXT,
        primary key (id)
     )
