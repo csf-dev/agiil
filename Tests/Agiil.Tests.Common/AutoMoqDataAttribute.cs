9f47e2a5dc4b40913d7bc8a7ed98e7d8caca9a19
Craig Fowler <craig@csf-dev.com>
2017-04-08 19:12:19 +0100

    Upgrade to NUnit test runner 3.6.1 (in case that helps)

--- Tests/Agiil.Tests.Common/AutoMoqDataAttribute.cs
+++ Tests/Agiil.Tests.Common/AutoMoqDataAttribute.cs
@@ -5,12 +5,10 @@ using Ploeh.AutoFixture.NUnit3;
 
 namespace Agiil.Tests.Common
 {
-#pragma warning disable CS1702 // Assuming assembly reference matches identity
   public class AutoMoqDataAttribute : AutoDataAttribute
   {
     public AutoMoqDataAttribute() : base(new Fixture().Customize(new AutoMoqCustomization()))
     {
     }
   }
-  #pragma warning restore CS1702 // Assuming assembly reference matches identity
 }
