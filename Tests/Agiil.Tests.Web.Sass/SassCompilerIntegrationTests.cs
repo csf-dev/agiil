1a58312982757dbb0a991557e1d0f53c93cdd8a8
Craig Fowler <craig@csf-dev.com>
2017-05-12 20:17:45 +0100

    Add integration tests and attempt with SharpScss library

---
+++ Tests/Agiil.Tests.Web.Sass/SassCompilerIntegrationTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using Agiil.Web.Sass;
+using NUnit.Framework;
+
+namespace Agiil.Tests.Web.Sass
+{
+  [TestFixture]
+  public class SassCompilerIntegrationTests
+  {
+    [Test]
+    public void Compile_compiles_a_scss_file_to_output_file()
+    {
+      // Arrange
+      var sut = GetSut();
+      var testDirectory = GetTestDirectory();
+      CreateSampleScssFilesWithImport(testDirectory);
+      testDirectory.Refresh();
+      var options = new SassCompilationOptions
+      {
+        SourceDirectory = testDirectory,
+        OutputDirectory = testDirectory
+      };
+
+      // Act
+      sut.Compile(options);
+
+      // Assert
+    }
+
+    ISassCompiler GetSut()
+    {
+      return new SassCompiler();
+    }
+
+    void CreateSampleScssFilesWithImport(DirectoryInfo testDirectory)
+    {
+      var fileOne = @"
+@import '_two';
+
+nav {
+  background: red;
+
+  ul {
+    background: yellow;
+  }
+}";
+      var fileTwo = @"
+p {
+  background: blue;
+}";
+      using(var writer = File.CreateText(Path.Combine(testDirectory.FullName, "one.scss")))
+      {
+        writer.WriteLine(fileOne);
+      }
+      using(var writer = File.CreateText(Path.Combine(testDirectory.FullName, "_two.scss")))
+      {
+        writer.WriteLine(fileTwo);
+      }
+    }
+
+    DirectoryInfo GetTestDirectory()
+    {
+      var output = TestFilesystem.GetTestTemporaryDirectory<SassCompilerIntegrationTests>();
+      if(output == null)
+        output = new DirectoryInfo(Environment.CurrentDirectory);
+
+      return output;
+    }
+  }
+}
---
+++ Tools/Agiil.Web.Sass/SharpScssConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using SharpScss;
+
+namespace Agiil.Web.Sass
+{
+  public class SharpScssConverter : ISassConverter
+  {
+    const string OutputMapExtension = ".map";
+
+    public void Convert(string inputPath, string outputPath, ScssOptions options)
+    {
+      var result = Scss.ConvertFileToCss(inputPath, options);
+      WriteCssFile(result.Css, outputPath);
+      WriteSourceMap(result.SourceMap, outputPath);
+    }
+
+    void WriteCssFile(string css, string outputPath)
+    {
+      using(var writer = File.CreateText(outputPath))
+      {
+        writer.WriteLine(css);
+      }
+    }
+
+    void WriteSourceMap(string map, string outputPath)
+    {
+      if(map == null)
+        return;
+      
+      using(var writer = File.CreateText(outputPath + OutputMapExtension))
+      {
+        writer.WriteLine(map);
+      }
+    }
+  }
+}
