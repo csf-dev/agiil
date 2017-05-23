8eedd132316855448b1f8736389197c6bbacb55c
Craig Fowler <craig@csf-dev.com>
2017-05-23 22:19:49 +0100

    Resolve #AG18 - Prevent validation errors when submitting content

--- Agiil.Web/App_Start/MvcGlobalFilterConfiguration.cs
+++ Agiil.Web/App_Start/MvcGlobalFilterConfiguration.cs
@@ -8,6 +8,9 @@ namespace Agiil.Web.App_Start
     public void RegisterFilters(GlobalFilterCollection filters)
     {
       filters.Add(new AuthorizeAttribute());
+
+      // This is safe only because I am using ZPT-Sharp to render all user-generated content!
+      filters.Add(new ValidateInputAttribute(false));
     }
   }
 }
