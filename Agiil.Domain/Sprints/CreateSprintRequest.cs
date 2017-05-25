53a954861a656dd44e20702e6edb3efc47c69875
Craig Fowler <craig@csf-dev.com>
2017-05-25 19:51:52 +0100

    WIP #AG12 - Add sprint entity type and supporting interfaces for create

---
+++ Agiil.Domain/Sprints/CreateSprintRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Agiil.Domain.Projects;
+using CSF.Entities;
+
+namespace Agiil.Domain.Sprints
+{
+  public class CreateSprintRequest
+  {
+    public IIdentity<Project> Project { get; set; }
+
+    public string Name { get; set; }
+
+    public DateTime? StartDate { get; set; }
+
+    public DateTime? EndDate { get; set; }
+  }
+}
---
+++ Agiil.Domain/Sprints/CreateSprintResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Agiil.Domain.Sprints
+{
+  public class CreateSprintResponse : IIndictesSuccess
+  {
+    public bool ProjectDoesNotExist { get; set; }
+
+    public bool NameIsInvalid { get; set; }
+
+    public bool EndDateMustNotBeBeforeStartDate { get; set; }
+
+    public bool IsSuccess
+      => !(ProjectDoesNotExist || NameIsInvalid || EndDateMustNotBeBeforeStartDate);
+  }
+}
