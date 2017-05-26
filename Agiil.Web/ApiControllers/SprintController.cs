7f2180255b3be3eca56227a1fcf70428dbee0d2e
Craig Fowler <craig@csf-dev.com>
2017-05-26 22:01:57 +0100

    Resolve #AG12 - Complete the work to create a new sprint

---
+++ Agiil.Web/ApiControllers/SprintController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.Http;
+using Agiil.Domain.Sprints;
+using Agiil.Web.Models.Sprints;
+
+namespace Agiil.Web.ApiControllers
+{
+  public class SprintController : ApiControllerBase
+  {
+    readonly Lazy<ISprintCreator> creator;
+
+    public NewSprintResult Put(NewSprintSpecification spec)
+    {
+      var request = Mapper.Map<CreateSprintRequest>(spec);
+      var response = creator.Value.Create(request);
+      return Mapper.Map<NewSprintResult>(response);
+    }
+
+    public SprintController(ApiControllerBaseDependencies deps,
+                            Lazy<ISprintCreator> creator) : base(deps)
+    {
+      if(creator == null)
+        throw new ArgumentNullException(nameof(creator));
+      this.creator = creator;
+    }
+  }
+}
