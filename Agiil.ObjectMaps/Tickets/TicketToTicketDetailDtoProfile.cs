d2183238a9a337043ad93941c3cafb7e603dddee
Craig Fowler <craig@csf-dev.com>
2017-07-29 10:45:48 +0100

    Fix usages of Model mapping resolvers

--- Agiil.ObjectMaps/Tickets/TicketToTicketDetailDtoProfile.cs
+++ Agiil.ObjectMaps/Tickets/TicketToTicketDetailDtoProfile.cs
@@ -9,13 +9,13 @@ namespace Agiil.ObjectMaps.Tickets
 {
   public class TicketToTicketDetailDtoProfile : Profile
   {
-    public TicketToTicketDetailDtoProfile()
+    public TicketToTicketDetailDtoProfile(TicketReferenceStringResolver ticketRefResolver)
     {
       CreateMap<Ticket,TicketDetailDto>()
         .ForMember(x => x.Id, o => o.ResolveUsing<IdentityValueResolver>())
         .ForMember(x => x.Creator, o => o.ResolveUsing(t => t.User.Username))
         .ForMember(x => x.Created, o => o.ResolveUsing(t => t.CreationTimestamp))
-        .ForMember(x => x.Reference, o => o.ResolveUsing<TicketReferenceStringResolver>())
+        .ForMember(x => x.Reference, o => o.ResolveUsing(ticketRefResolver))
         .AfterMap((ticket, dto) => {
           dto.Comments = dto.Comments.OrderBy(x => x.Timestamp).ToArray();
         })
--- Agiil.ObjectMaps/Tickets/TicketToTicketSummaryDtoProfile.cs
+++ Agiil.ObjectMaps/Tickets/TicketToTicketSummaryDtoProfile.cs
@@ -8,13 +8,13 @@ namespace Agiil.ObjectMaps.Tickets
 {
   public class TicketToTicketSummaryDtoProfile : Profile
   {
-    public TicketToTicketSummaryDtoProfile()
+    public TicketToTicketSummaryDtoProfile(TicketReferenceStringResolver ticketRefResolver)
     {
       CreateMap<Ticket,TicketSummaryDto>()
         .ForMember(x => x.Id, o => o.ResolveUsing<IdentityValueResolver>())
         .ForMember(x => x.Creator, o => o.ResolveUsing(t => t.User.Username))
         .ForMember(x => x.Created, o => o.ResolveUsing(t => t.CreationTimestamp))
-        .ForMember(x => x.Reference, o => o.ResolveUsing<TicketReferenceStringResolver>())
+        .ForMember(x => x.Reference, o => o.ResolveUsing(ticketRefResolver))
         ;
     }
   }
