2c6a8686561343f3735035887ddaee2bf668b355
Craig Fowler <craig@csf-dev.com>
2017-09-27 21:57:49 +0100

    Resolve #AG24 - Sanitize rendered markdown HTML

--- Agiil.Web.Rendering.Impl/CommonMarkMarkdownConverter.cs
+++ Agiil.Web.Rendering.Impl/CommonMarkMarkdownConverter.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using CommonMark;
+
 namespace Agiil.Web.Rendering
 {
-  public class CommonMarkHtmlRenderer : IHtmlRenderer
+  public class CommonMarkMarkdownConverter : IMarkdownConverter
   {
     public virtual string GetHtml(string markdown)
     {
       if(markdown == null) return null;
-      return CommonMark.CommonMarkConverter.Convert(markdown);
+      return CommonMarkConverter.Convert(markdown);
     }
   }
 }
---
+++ Agiil.Web.Rendering.Impl/HtmlRenderingService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Agiil.Web.Rendering
+{
+  public class HtmlRenderingService : IHtmlRenderer
+  {
+    readonly IMarkupSanitizer sanitizer;
+    readonly IMarkdownConverter converter;
+
+    public string GetHtml(string markdown)
+    {
+      var htmlMarkup = converter.GetHtml(markdown);
+      return sanitizer.Sanitize(htmlMarkup);
+    }
+
+    public HtmlRenderingService(IMarkupSanitizer sanitizer, IMarkdownConverter converter)
+    {
+      if(converter == null)
+        throw new ArgumentNullException(nameof(converter));
+      if(sanitizer == null)
+        throw new ArgumentNullException(nameof(sanitizer));
+      
+      this.converter = converter;
+      this.sanitizer = sanitizer;
+    }
+  }
+}
