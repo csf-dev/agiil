4dfdf9ae95782198f2cc79ba963826f3ac8bf5c6
Craig Fowler <craig@csf-dev.com>
2017-10-04 20:45:47 +0100

    Refactor some tests to use DI more clearly

---
+++ Tests/Agiil.Tests.Common/CachingDomainContainerFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Agiil.Bootstrap;
+using Autofac;
+
+namespace Agiil.Tests
+{
+  public class CachingDomainContainerFactory : DomainContainerFactory
+  {
+    static readonly IAutofacContainerFactory defaultInstance;
+
+    readonly object syncRoot;
+    IContainer container;
+
+    public override IContainer GetContainer()
+    {
+      lock(syncRoot)
+      {
+        if(container == null)
+          container = base.GetContainer();
+      }
+
+      return container;
+    }
+
+    public CachingDomainContainerFactory()
+    {
+      syncRoot = new object();
+    }
+
+    static CachingDomainContainerFactory()
+    {
+      defaultInstance = new CachingDomainContainerFactory();
+    }
+
+    public static IAutofacContainerFactory Default => defaultInstance;
+  }
+}
