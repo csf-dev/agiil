e9d64084da8d060ea88df8dfe5fafaf3323d9e2c
Craig Fowler <craig@csf-dev.com>
2017-10-04 22:30:36 +0100

    Fixes to #AG52 - Select ticket type on creation

---
+++ Agiil.ObjectMaps/Tickets/TicketTypeToTicketTypeDtoProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Agiil.Domain.Tickets;
+using Agiil.Web.Models.Tickets;
+using AutoMapper;
+
+namespace Agiil.ObjectMaps.Tickets
+{
+  public class TicketTypeToTicketTypeDtoProfile : Profile
+  {
+    public TicketTypeToTicketTypeDtoProfile()
+    {
+      CreateMap<TicketType,TicketTypeDto>();
+    }
+  }
+}
