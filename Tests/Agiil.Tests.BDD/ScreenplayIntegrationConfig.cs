19930cfa60d61afa33c7daba255322e9321d511c
Craig Fowler <craig@csf-dev.com>
2018-02-24 13:49:40 +0000

    WIP Screenplay version - Fix issues in Screenplay integration config

--- Tests/Agiil.Tests.BDD/ScreenplayIntegrationConfig.cs
+++ Tests/Agiil.Tests.BDD/ScreenplayIntegrationConfig.cs
@@ -8,6 +8,7 @@ using CSF.Screenplay.Selenium.Abilities;
 using System.IO;
 using CSF.Screenplay.Selenium.Reporting;
 using CSF.Screenplay.SpecFlow;
+using System;
 
 [assembly: ScreenplayAssembly(typeof(Agiil.Tests.BDD.ScreenplayIntegrationConfig))]
 
@@ -18,6 +19,7 @@ namespace Agiil.Tests.BDD
     public void Configure(IIntegrationConfigBuilder builder)
     {
       builder.UseCast();
+      builder.UseStage();
       builder.UseReporting(reporting => {
         reporting
           .SubscribeToActorsCreatedInCast()
@@ -38,8 +40,33 @@ namespace Agiil.Tests.BDD
     void WriteReport(IObjectFormattingService formatter, Report report)
     {
       var directory = TestFilesystem.GetTestTemporaryDirectory();
+      WriteTextReport(formatter, report, directory);
+      WriteHtmlReport(formatter, report, directory);
+    }
+
+    void WriteTextReport(IObjectFormattingService formatter, Report report, DirectoryInfo directory)
+    {
       var reportPath = Path.Combine(directory.FullName, $"Agiil.Tests.BDD.report.txt");
       TextReportWriter.WriteToFile(report, reportPath, formatter);
+    }
+
+    void WriteHtmlReport(IObjectFormattingService formatter, Report report, DirectoryInfo directory)
+    {
+      var reportPath = Path.Combine(directory.FullName, $"Agiil.Tests.BDD.report.html");
+      try
+      {
+        using(var writer = new StreamWriter(reportPath))
+        {
+          var reportWriter = new HtmlReportWriter(writer, formatter);
+          reportWriter.Write(report);
+        }
+      }
+      catch(Exception ex)
+      {
+        Console.Error.WriteLine("Error whilst writing HTML report");
+        Console.Error.WriteLine(ex);
+        throw;
+      }
     }
   }
 }
