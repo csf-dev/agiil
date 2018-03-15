dac0233099eb2726340a87617d9c4f2d7c2fbc99
Craig Fowler <craig@csf-dev.com>
2018-03-15 19:51:43 +0000

    WIP #AG45 - Add web project types for version info

---
+++ Agiil.Web/Services/VersionInfoProvider.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Agiil.Domain;
+
+namespace Agiil.Web.Services
+{
+  public class VersionInfoProvider : IProvidesVersionInformation
+  {
+    public virtual IReadOnlyCollection<ComponentVersionInfo> GetComponentVersions()
+    {
+      return new [] {
+        GetVersionFromAssemblyWhichContains<Global>(),
+        GetVersionFromAssemblyWhichContains<IDomainAssemblyMarker>(),
+      };
+    }
+
+    public virtual string GetHumanReadableProductVersion()
+    {
+      var assembly = Assembly.GetExecutingAssembly();
+      return GetInformationalVersion(assembly) ?? GetAssemblyVersion(assembly);
+    }
+
+    protected ComponentVersionInfo GetVersionFromAssemblyWhichContains<T>()
+    {
+      var assemblyName = typeof(T).Assembly.GetName();
+      return new ComponentVersionInfo {
+        ComponentName = assemblyName.Name,
+        Version = assemblyName.Version.ToString(),
+      };
+    }
+
+    protected string GetInformationalVersion(Assembly assembly)
+    {
+      return assembly?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+    }
+
+    protected string GetAssemblyVersion(Assembly assembly)
+    {
+      return assembly?.GetName().Version?.ToString();
+    }
+  }
+}
