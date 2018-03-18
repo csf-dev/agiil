e64a153e29aa9b94fb06678aafccfb4ec0e0caf7
Craig Fowler <craig@csf-dev.com>
2018-03-18 18:27:26 +0000

    WIP #AG29 - Add new JSON converter for identity instances

--- Agiil.Web/App_Start/WebApiModelBindingRules.cs
+++ Agiil.Web/App_Start/WebApiModelBindingRules.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Web.Http;
 using System.Web.Http.Controllers;
 using Agiil.Domain.Tickets;
@@ -14,6 +15,28 @@ namespace Agiil.Web.App_Start
       config
         .ParameterBindingRules
         .Add(typeof(IIdentity<Ticket>), p => p.BindWithModelBinding());
+
+    public static bool IsIdentityType(Type type) => GetEntityType(type) != null;
+
+    public static Type GetEntityType(Type identityType)
+    {
+      if(identityType == null) return null;
+
+      var implementedInterfaces = identityType
+        .GetInterfaces()
+        .Union(new [] { identityType });
+
+      var genericIdentityInterface = (from iface in implementedInterfaces
+                                      where
+                                        iface.IsInterface
+                                        && iface.IsGenericType
+                                        && iface.GetGenericTypeDefinition() == typeof(IIdentity<>)
+                                      select iface)
+        .SingleOrDefault();
+
+      if(genericIdentityInterface == null) return null;
+
+      return genericIdentityInterface.GenericTypeArguments[0];
     }
   }
 }
