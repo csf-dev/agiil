022ed3c09536e6f6fb5918f157a9b46a13375f62
Craig Fowler <craig@csf-dev.com>
2018-03-18 13:26:10 +0000

    WIP #AG28 - Add service to get the root URI for the application

---
+++ Agiil.Web/Services/RootOfTheRequestDomainUriProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+using Agiil.Domain;
+
+namespace Agiil.Web.Services
+{
+  public class RootOfTheRequestDomainUriProvider : IProvidesApplicationBaseUri
+  {
+    public Uri GetBaseUri()
+    {
+      var requestUri = GetRequestUri();
+      var uriString = requestUri.GetLeftPart(UriPartial.Authority);
+      return new Uri(uriString);
+    }
+
+    protected virtual Uri GetRequestUri()
+    {
+      var context = GetHttpContext();
+      return context?.Request?.Url;
+    }
+
+    HttpContext GetHttpContext() => HttpContext.Current;
+  }
+}
---
+++ Tests/Agiil.Tests.Web/Services/RootOfTheRequestDomainUriProviderTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Agiil.Web.Services;
+using NUnit.Framework;
+
+namespace Agiil.Tests.Web.Services
+{
+  [TestFixture,Parallelizable]
+  public class RootOfTheRequestDomainUriProviderTests
+  {
+    [Test]
+    public void GetBaseUri_returns_root_of_the_domain_when_the_request_has_a_long_path()
+    {
+      // Arrange
+      var requestUri = new Uri("http://example.com/foo/bar/baz");
+      var expectedUri = new Uri("http://example.com/");
+      var sut = new InjectedUriProvider(requestUri);
+
+      // Act
+      var result = sut.GetBaseUri();
+
+      // Assert
+      Assert.That(result, Is.EqualTo(expectedUri));
+    }
+
+    [Test]
+    public void GetBaseUri_returns_root_of_the_domain_when_the_request_has_a_port()
+    {
+      // Arrange
+      var requestUri = new Uri("http://example.com:8080/foo");
+      var expectedUri = new Uri("http://example.com:8080/");
+      var sut = new InjectedUriProvider(requestUri);
+
+      // Act
+      var result = sut.GetBaseUri();
+
+      // Assert
+      Assert.That(result, Is.EqualTo(expectedUri));
+    }
+
+    [Test]
+    public void GetBaseUri_returns_root_of_the_domain_when_the_request_is_https()
+    {
+      // Arrange
+      var requestUri = new Uri("https://example.com/foo");
+      var expectedUri = new Uri("https://example.com/");
+      var sut = new InjectedUriProvider(requestUri);
+
+      // Act
+      var result = sut.GetBaseUri();
+
+      // Assert
+      Assert.That(result, Is.EqualTo(expectedUri));
+    }
+
+    class InjectedUriProvider : ApplicationRootUriProvider
+    {
+      readonly Uri injectedRequestUri;
+
+      protected override Uri GetRequestUri()
+      {
+        return injectedRequestUri ?? base.GetRequestUri();
+      }
+
+      public InjectedUriProvider(Uri injectedRequestUri)
+      {
+        this.injectedRequestUri = injectedRequestUri;
+      }
+    }
+  }
+}
