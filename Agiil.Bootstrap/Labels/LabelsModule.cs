a1c149d5028a582365b77ad7f3a141e6f664aec5
Craig Fowler <craig@csf-dev.com>
2018-05-09 20:05:05 +0100

    WIP #AG47 - Add bootstrap module for services relating to labels

---
+++ Agiil.Bootstrap/Labels/LabelsModule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Agiil.Domain.Labels;
+using Autofac;
+
+namespace Agiil.Bootstrap.Labels
+{
+  public class LabelsModule : NamespaceModule
+  {
+    protected override string Namespace => typeof(Label).Namespace;
+
+    protected override IEnumerable<Type> TypesNotToRegisterAutomatically
+      => new [] {
+        typeof(ExistingAndNewLabelProvider),
+        typeof(ExistingLabelProvider),
+        typeof(NewLabelProvider),
+      };
+
+		protected override void Load(ContainerBuilder builder)
+		{
+      base.Load(builder);
+      RegisterLabelProvider(builder);
+		}
+
+    void RegisterLabelProvider(ContainerBuilder builder)
+    {
+      builder.RegisterType<ExistingLabelProvider>().AsSelf();
+      builder.RegisterType<NewLabelProvider>().AsSelf();
+
+      builder
+        .Register(ctx => {
+          var existingLabelProvider = ctx.Resolve<ExistingLabelProvider>();
+          var newLabelProvider = ctx.Resolve<NewLabelProvider>();
+          var nameParser = ctx.Resolve<IParsesLabelNames>();
+          return new ExistingAndNewLabelProvider(existingLabelProvider, newLabelProvider, nameParser);
+        })
+        .As<IGetsLabels>();
+    }
+	}
+}
