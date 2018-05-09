cbc1ef231c170b06a5a8577c078af9c5f0731c43
Craig Fowler <craig@csf-dev.com>
2018-05-09 20:07:13 +0100

    WIP #AG47 - Add new attribute to create in-memory entity data for tests
    This will make testing easier by making it easy to generate an in-memory
    data access layer.

---
+++ Tests/Agiil.Tests.Common/Attributes/InMemoryAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using Agiil.Tests.Autofixture;
+using CSF.Data.Entities;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.NUnit3;
+
+namespace Agiil.Tests.Attributes
+{
+  public class InMemoryAttribute : CustomizeAttribute
+  {
+    public bool GenerateId { get; set; }
+
+    public override ICustomization GetCustomization(ParameterInfo parameter)
+    {
+      if(!typeof(IEntityData).IsAssignableFrom(parameter.ParameterType))
+        throw new ArgumentException($"Decorated parameter must implement {nameof(IEntityData)}.");
+      
+      return new InMemoryEntityDataCustomization(GenerateId);
+    }
+
+    public InMemoryAttribute()
+    {
+      GenerateId = true;
+    }
+  }
+}
