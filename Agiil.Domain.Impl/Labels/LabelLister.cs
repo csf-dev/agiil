f708fb3a7b95eec88b22e1622db76d5d489a0125
Craig Fowler <craig@csf-dev.com>
2018-05-12 15:14:20 +0100

    WIP #AG110 - Add back-end logic to get list of labels

---
+++ Agiil.Domain.Impl/Labels/LabelLister.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Agiil.Domain.Tickets;
+using CSF.Data.Entities;
+using CSF.Data.Specifications;
+
+namespace Agiil.Domain.Labels
+{
+  public class LabelLister : IGetsListOfLabels
+  {
+    readonly IEntityData data;
+    readonly IsOpen isOpen;
+    readonly IsClosed isClosed;
+
+    public IReadOnlyList<ListedLabel> GetAllLabels()
+    {
+      return (from label in data.Query<Label>()
+              let openCount = label.Tickets.Count(isOpen)
+              let closedCount = label.Tickets.Count(isClosed)
+              orderby label.Name
+              select new ListedLabel {
+                Name = label.Name,
+                CountOfOpenTickets = openCount,
+                CountOfClosedTickets = closedCount
+              })
+        .ToArray();
+    }
+
+    public LabelLister(IEntityData data, IsOpen isOpen, IsClosed isClosed)
+    {
+      if(isClosed == null)
+        throw new ArgumentNullException(nameof(isClosed));
+      if(isOpen == null)
+        throw new ArgumentNullException(nameof(isOpen));
+      if(data == null)
+        throw new ArgumentNullException(nameof(data));
+
+      this.data = data;
+      this.isOpen = isOpen;
+      this.isClosed = isClosed;
+    }
+  }
+}
