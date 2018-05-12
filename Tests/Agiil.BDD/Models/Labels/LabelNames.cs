012029efe4fc642624610d9d2a7ba8d7315fb41b
Craig Fowler <craig@csf-dev.com>
2018-05-12 14:17:18 +0100

    WIP #AG110 - Complete specs and test implementation for label list

---
+++ Tests/Agiil.BDD/Models/Labels/LabelNames.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Agiil.BDD.Models.Labels
+{
+  public class LabelNames
+  {
+    public ICollection<string> Names { get; }
+
+    public LabelNames()
+    {
+      Names = new List<string>();
+    }
+  }
+}
