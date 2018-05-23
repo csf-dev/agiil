a409330f799edbb8e8d5e7040f06d23043f78c07
Craig Fowler <craig@csf-dev.com>
2018-05-23 19:33:41 +0100

    WIP #AG54 - Add logic to allow anonymous access to maintenance controllers
    This logic is now protected by a config setting which enables/disables
    all access to these controllers.  In release builds the access is disabled,
    but for debug builds it's permitted.

--- Agiil.Web/Controllers/DatabaseBackupController.cs
+++ Agiil.Web/Controllers/DatabaseBackupController.cs
@@ -1,20 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Agiil.Domain.Data;
+using Agiil.Web.Services.Data;
 
 namespace Agiil.Web.Controllers
 {
+  [AllowAnonymous]
   public class DatabaseBackupController : Controller
   {
+    readonly IGetsDatabaseMaintenanceSecurity securityProvider;
     readonly ITakesDatabaseBackup backupService;
     readonly IRestoresDatabaseBackup backupRestorer;
 
     [HttpPost]
     public ActionResult TakeBackup(string name)
     {
+      if(!securityProvider.IsDatabaseMaintenancePermitted())
+        return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+
       backupService.TakeDatabaseBackup(name);
       return RedirectToAction(nameof(DatabaseController.Index), this.GetName<DatabaseController>());
     }
@@ -22,25 +29,35 @@ namespace Agiil.Web.Controllers
     [HttpGet]
     public ActionResult Index(string filename)
     {
+      if(!securityProvider.IsDatabaseMaintenancePermitted())
+        return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+
       return View((object) filename);
     }
 
     [HttpPost]
     public ActionResult ConfirmRestoreBackup(string filename)
     {
+      if(!securityProvider.IsDatabaseMaintenancePermitted())
+        return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+
       backupRestorer.RestoreDatabaseBackup(filename);
       return RedirectToAction(nameof(DatabaseController.Index), this.GetName<DatabaseController>());
     }
 
     public DatabaseBackupController(ITakesDatabaseBackup backupService,
-                                    IRestoresDatabaseBackup backupRestorer)
+                                    IRestoresDatabaseBackup backupRestorer,
+                                    IGetsDatabaseMaintenanceSecurity securityProvider)
     {
+      if(securityProvider == null)
+        throw new ArgumentNullException(nameof(securityProvider));
       if(backupRestorer == null)
         throw new ArgumentNullException(nameof(backupRestorer));
       if(backupService == null)
         throw new ArgumentNullException(nameof(backupService));
 
       this.backupRestorer = backupRestorer;
+      this.securityProvider = securityProvider;
       this.backupService = backupService;
     }
   }
---
+++ Agiil.Web/Services/Data/AppSettingsDatabaseMaintenanceSecurityProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Configuration;
+
+namespace Agiil.Web.Services.Data
+{
+  public class AppSettingsDatabaseMaintenanceSecurityProvider : IGetsDatabaseMaintenanceSecurity
+  {
+    internal const string AllowDatabaseMaintenanceKey = "AllowDatabaseMaintenance";
+
+    public bool IsDatabaseMaintenancePermitted()
+    {
+      var setting = ConfigurationManager.AppSettings[AllowDatabaseMaintenanceKey];
+      return String.Equals(setting, Boolean.TrueString, StringComparison.InvariantCultureIgnoreCase);
+    }
+  }
+}
