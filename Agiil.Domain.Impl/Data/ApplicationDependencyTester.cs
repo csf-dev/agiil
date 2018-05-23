9dbe9eeb475507e6edbd45b3a2d7b08f6f630402
Craig Fowler <craig@csf-dev.com>
2018-05-23 19:25:38 +0100

    WIP #AG54 - Add a service which tests for the application dependencies
    Right now this is just the database, but in future it could be "any
    execution dependency" which should prevent the application from running
    if it were missing.
    
    Where the tests fail, the exact nature of the failure is recorded in the
    application log file at either the WARN or FATAL levels (depending on
    whether the problem can be fixed in maintenance mode or not).

---
+++ Agiil.Domain.Impl/Data/ApplicationDependencyTester.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace Agiil.Domain.Data
+{
+  public class ApplicationDependencyTester : ITestsApplicationDependencies
+  {
+    readonly ITestsThatDatabaseIsWorking databaseTester;
+    readonly log4net.ILog logger;
+
+    public AppDependencyTestResult TestApplicationDependencies()
+    {
+      var result = new AppDependencyTestResult {
+        IsApplicationViableForMaintenance = true,
+        IsApplicationOKToUse = true
+      };
+
+      PerformDatabaseChecks(result);
+
+      return result;
+    }
+
+    void PerformDatabaseChecks(AppDependencyTestResult result)
+    {
+      PerformCriticalChecks(result);
+      if(!result.IsApplicationViableForMaintenance) return;
+      PerformMaintenanceChecks(result);
+    }
+
+    void PerformCriticalChecks(AppDependencyTestResult result)
+    {
+      result.ConnectionStringIsMissing = !databaseTester.IsConnectionStringAvailable();
+      result.CannotConnectToDatabase = !databaseTester.IsDatabaseConnectionPossible();
+
+      if(result.ConnectionStringIsMissing)
+      {
+        logger.Fatal("The application connection string is missing");
+        result.IsApplicationViableForMaintenance = false;
+        result.IsApplicationOKToUse = false;
+      }
+
+      if(result.CannotConnectToDatabase)
+      {
+        logger.Fatal("Cannot connect to the application database");
+        result.IsApplicationViableForMaintenance = false;
+        result.IsApplicationOKToUse = false;
+      }
+    }
+
+    void PerformMaintenanceChecks(AppDependencyTestResult result)
+    {
+      result.DatabaseRequiresUpgrade = !databaseTester.IsDatabaseFullyUpgraded();
+
+      if(result.DatabaseRequiresUpgrade)
+      {
+        logger.Error("The application database requires upgrades");
+        result.IsApplicationOKToUse = false;
+      }
+    }
+
+    public ApplicationDependencyTester(ITestsThatDatabaseIsWorking databaseTester)
+    {
+      if(databaseTester == null)
+        throw new ArgumentNullException(nameof(databaseTester));
+      
+      this.databaseTester = databaseTester;
+      logger = log4net.LogManager.GetLogger(GetType());
+    }
+  }
+}
---
+++ Agiil.Domain/Data/AppDependencyTestResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Agiil.Domain.Data
+{
+  public class AppDependencyTestResult
+  {
+    public bool ConnectionStringIsMissing { get; set; }
+
+    public bool CannotConnectToDatabase { get; set; }
+
+    public bool DatabaseRequiresUpgrade { get; set; }
+
+    public bool IsApplicationOKToUse { get; set; }
+
+    public bool IsApplicationViableForMaintenance { get; set; }
+  }
+}
