8d7cb10148960ce75641a56cb8ce098ebc031db3
Craig Fowler <craig@csf-dev.com>
2018-05-28 18:58:35 +0100

    WIP #AG71 - Add a mechanism to explicitly get the extensions in use

--- Agiil.Web.Rendering.Impl/MarkdigPipelineFactory.cs
+++ Agiil.Web.Rendering.Impl/MarkdigPipelineFactory.cs
@@ -5,11 +5,26 @@ namespace Agiil.Web.Rendering
 {
   public class MarkdigPipelineFactory
   {
+    readonly MarkdownExtensionsProvider extensionsProvider;
+
     public MarkdownPipeline GetMarkdownPipeline()
     {
       var builder = new MarkdownPipelineBuilder();
 
+      var extensions = extensionsProvider.GetExtensions();
+      foreach(var extension in extensions)
+      {
+        builder.Use(extension);
+      }
+
       return builder.Build();
+    }
+
+    public MarkdigPipelineFactory(MarkdownExtensionsProvider extensionsProvider)
+    {
+      if(extensionsProvider == null)
+        throw new ArgumentNullException(nameof(extensionsProvider));
+      this.extensionsProvider = extensionsProvider;
     }
   }
 }
---
+++ Agiil.Web.Rendering.Impl/MarkdownExtensionsProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Markdig;
+
+namespace Agiil.Web.Rendering
+{
+  public class MarkdownExtensionsProvider
+  {
+    public IEnumerable<IMarkdownExtension> GetExtensions()
+    {
+      return Enumerable.Empty<IMarkdownExtension>();
+    }
+  }
+}
