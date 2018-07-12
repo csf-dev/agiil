52e2a34c0297dfc8f5b8c9af95e88413026ab348
Craig Fowler <craig@csf-dev.com>
2018-07-12 21:34:00 +0100

    Fixup add object model of nodes to support

---
+++ Agiil.Domain.Impl/TicketSearch/LogicalAndNode.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Agiil.Domain.TicketSearch
+{
+  /// <summary>
+  /// Indicates that ALL of the <see cref="IHasChildNodes.Children"/> of the current node must be true in order to
+  /// consider this criterion satisfied.
+  /// </summary>
+  public class LogicalAndNode : SearchNode
+  {
+  }
+}
