d87f7a99eb7979fcae0ea961d5c4b633e2d94a63
Craig Fowler <craig@csf-dev.com>
2018-07-14 20:17:04 +0100

    Trivial - add comment

--- Agiil.Domain/Tickets/TicketListRequest.cs
+++ Agiil.Domain/Tickets/TicketListRequest.cs
@@ -7,6 +7,10 @@ namespace Agiil.Domain.Tickets
   {
     public SearchModel CriteriaModel { get; }
 
+    // In the future this will also have:
+    // * An ordering model
+    // * A model indicating which columns we wish to display in the output
+
     public TicketListRequest()
     {
       CriteriaModel = new SearchModel();
