d37a7a0af4b1bb5292488a8a1623edf3b8fb3034
Craig Fowler <craig@csf-dev.com>
2018-07-15 19:28:45 +0100

    WIP #89 - Alter project as to not break MonoDevelop's build process

---
+++ Agiil.QueryLanguage.Antlr/EmptyClass.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Agiil.QueryLanguage.Antlr
+{
+  public class EmptyClass
+  {
+    // This is an intentionally empty class, because this library contains no .NET functionality
+  }
+}
