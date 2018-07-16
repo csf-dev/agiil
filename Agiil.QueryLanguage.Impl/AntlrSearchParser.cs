a3eb9bc76c175df5bb93ec18e7221c5a3e294e43
Craig Fowler <craig@csf-dev.com>
2018-07-16 21:42:03 +0100

    WIP #AG89 - Beginnings of the search parser

---
+++ Agiil.QueryLanguage.Impl/AntlrSearchParser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Agiil.QueryLanguage.Impl
+{
+  public class AntlrSearchParser : IGetsSearch
+  {
+    public Search GetSearch(string text)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
