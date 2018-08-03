15e254a9148315175c47ff2ce475d31a9a0de117
Craig Fowler <craig@csf-dev.com>
2018-08-03 21:16:02 +0100

    WIP #AG90 - Fix mapping for enumerations (strings)

---
+++ Agiil.Data/ConventionMappings/EnumerationPropertyMapping.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using Agiil.Data.MappingProviders;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Type;
+
+namespace Agiil.Data.ConventionMappings
+{
+  public class EnumerationPropertyMapping : IConventionMapping
+  {
+    static readonly Type OpenGenericEnumType = typeof(EnumStringType<>);
+
+    public void ApplyMapping(ConventionModelMapper mapper)
+    {
+      mapper.BeforeMapProperty += (modelInspector, member, propertyCustomizer) => {
+        var propertyInfo = member.LocalMember as PropertyInfo;
+        if(propertyInfo == null) return;
+
+        if(!propertyInfo.PropertyType.IsEnum) return;
+
+        propertyCustomizer.Type(GetEnumerationType(propertyInfo));
+      };
+    }
+
+    IType GetEnumerationType(PropertyInfo property)
+    {
+      var enumType = OpenGenericEnumType.MakeGenericType(property.PropertyType);
+      return (IType) Activator.CreateInstance(enumType);
+    }
+  }
+}
