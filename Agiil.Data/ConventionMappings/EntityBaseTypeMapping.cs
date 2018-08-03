40e01fc28e01648fd602a4b2334609982feb27e7
Craig Fowler <craig@csf-dev.com>
2018-08-03 21:52:14 +0100

    Trivial - add note about mad docs on the Internet (including Fabio Maulo's blog)

--- Agiil.Data/ConventionMappings/EntityBaseTypeMapping.cs
+++ Agiil.Data/ConventionMappings/EntityBaseTypeMapping.cs
@@ -12,6 +12,15 @@ namespace Agiil.Data.ConventionMappings
       mapper.IsRootEntity(IsRootEntity);
     }
 
+    /// <summary>
+    /// Watch out - there is a lot of documentation on the Internet which says that the "root entity" should be
+    /// an unmapped base type for entities (layer subtype).  This is incorrect, this is supposed to be true
+    /// for a mapped entity which is the at the root of an entity hierarchy mapping.  In other words, this should be
+    /// true for all entities which are not subclasses of other entities.
+    /// </summary>
+    /// <returns><c>true</c>, if the type is a root entity, <c>false</c> otherwise.</returns>
+    /// <param name="type">A type to consider.</param>
+    /// <param name="isDeclaredAsRootAlready">If set to <c>true</c> then the entity has been declared as a root entity already.</param>
     bool IsRootEntity(Type type, bool isDeclaredAsRootAlready)
     {
       if(type.IsInterface) return false;
