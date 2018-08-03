d20019bf77c84efbaaed1954d490e41b8107c88d
Craig Fowler <craig@csf-dev.com>
2018-08-03 21:51:24 +0100

    WIP #AG90 - Add domain entities for relationships

---
+++ Agiil.Domain/Tickets/DirectionalRelationship.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CSF.Entities;
+
+namespace Agiil.Domain.Tickets
+{
+  [BaseType(typeof(Relationship))]
+  public class DirectionalRelationship : Relationship
+  {
+    public virtual string SecondarySummary { get; set; }
+
+    public DirectionalRelationship() : base(RelationshipType.Directional) {}
+  }
+}
---
+++ Agiil.Domain/Tickets/NonDirectionalRelationship.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using CSF.Entities;
+
+namespace Agiil.Domain.Tickets
+{
+  [BaseType(typeof(Relationship))]
+  public class NonDirectionalRelationship : Relationship
+  {
+    public NonDirectionalRelationship() : base(RelationshipType.NonDirectional) {}
+  }
+}
