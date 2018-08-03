f0827faf06e60b84337578b137cf92d5e92b6ca2
Craig Fowler <craig@csf-dev.com>
2018-08-03 21:51:50 +0100

    WIP #AG90 - Add mapping for the relationship entities

---
+++ Agiil.Data/ClassMappings/RelationshipMapping.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Agiil.Data.MappingProviders;
+using Agiil.Domain.Tickets;
+using CSF.Reflection;
+using NHibernate.Mapping.ByCode;
+
+namespace Agiil.Data.ClassMappings
+{
+  public class RelationshipMapping : IConventionMapping
+  {
+    readonly IDbNameFormatter nameFormatter;
+
+    public void ApplyMapping(ConventionModelMapper mapper)
+    {
+      MapRelationship(mapper);
+      MapDirectionalRelationship(mapper);
+      MapNonDirectionalRelationship(mapper);
+    }
+
+    void MapRelationship(ConventionModelMapper mapper)
+    {
+      mapper.Class<Relationship>(map => {
+        map.Discriminator(d => {
+          d.Column(nameFormatter.GetColumnName(Reflect.Property<Relationship>(x => x.Type)));
+          d.NotNullable(true);
+        });
+      });
+    }
+
+    void MapDirectionalRelationship(ConventionModelMapper mapper)
+    {
+      mapper.Subclass<DirectionalRelationship>(map => {
+        map.DiscriminatorValue(RelationshipType.Directional);
+      });
+    }
+
+    void MapNonDirectionalRelationship(ConventionModelMapper mapper)
+    {
+      mapper.Subclass<NonDirectionalRelationship>(map => {
+          map.DiscriminatorValue(RelationshipType.NonDirectional);
+      });
+    }
+
+    public RelationshipMapping(IDbNameFormatter nameFormatter)
+    {
+      if(nameFormatter == null)
+        throw new ArgumentNullException(nameof(nameFormatter));
+      this.nameFormatter = nameFormatter;
+    }
+  }
+}
