89ade373cb0ae43c8c1cc1961e4a39fb14b5f173
Craig Fowler <craig@csf-dev.com>
2018-08-08 21:47:41 +0100

    WIP #AG90 - Refactor the way that the ticket controller gets the model

--- Agiil.Web/Controllers/TicketController.cs
+++ Agiil.Web/Controllers/TicketController.cs
@@ -5,6 +5,7 @@ using Agiil.Domain.Sprints;
 using Agiil.Domain.Tickets;
 using Agiil.Web.Models.Sprints;
 using Agiil.Web.Models.Tickets;
+using Agiil.Web.Services.Tickets;
 using AutoMapper;
 using CSF.Entities;
 
@@ -12,15 +13,14 @@ namespace Agiil.Web.Controllers
 {
   public class TicketController : Controller
   {
-    const string 
+    internal const string 
       EditTicketResponseKey = "Edit ticket response",
       EditTicketSpecKey = "Edit ticket specification",
       SuccessfulEditKey = "Successful edit";
 
     readonly ITicketDetailService ticketDetailService;
     readonly Lazy<IHandlesEditTicketRequest> editor;
-    readonly Lazy<ISprintLister> sprintLister;
-    readonly Lazy<ITicketTypeProvider> typeProvider;
+    readonly Func<TempDataDictionary,IGetsEditTicketModel> editTicketModelFactory;
     readonly IMapper mapper;
 
     public ActionResult Index(IIdentity<Ticket> id)
@@ -43,7 +43,8 @@ namespace Agiil.Web.Controllers
       if(ReferenceEquals(ticket, null))
         return HttpNotFound();
 
-      var model = GetEditTicketModel(ticket);
+      var editModelFactory = editTicketModelFactory(TempData);
+      var model = editModelFactory.GetEditTicketModel(ticket);
 
       return View (model);
     }
@@ -81,47 +82,15 @@ namespace Agiil.Web.Controllers
       return model;
     }
 
-    void PopulateCreateCommentModelProperties(TicketDetailModel model)
-    {
-      if(model == null)
-        throw new ArgumentNullException(nameof(model));
-
-      model.AddCommentSpecification = TempData.TryGet<AddCommentSpecification>(CommentController.CommentSpecKey);
-      model.AddCommentResponse = TempData.TryGet<AddCommentResponse>(CommentController.CommentResponseKey);
-    }
-
-    EditTicketModel GetEditTicketModel(Ticket ticket)
-    {
-      var model = new EditTicketModel();
-      model.Ticket = mapper.Map<TicketDetailDto>(ticket);
-      model.Response = TempData.TryGet<Models.Tickets.EditTicketResponse>(EditTicketResponseKey);
-      model.Specification = TempData.TryGet<EditTicketSpecification>(EditTicketSpecKey);
-      model.AvailableSprints = sprintLister
-        .Value
-        .GetSprints()
-        .Select(x => mapper.Map<SprintSummaryDto>(x))
-        .ToList();
-      model.AvailableTicketTypes = typeProvider
-        .Value
-        .GetTicketTypes()
-        .Select(x => mapper.Map<TicketTypeDto>(x))
-        .ToList();
-      
-      return model;
-    }
-
     public TicketController(ITicketDetailService ticketDetailService,
                             Lazy<IHandlesEditTicketRequest> editor,
-                            Lazy<ISprintLister> sprintLister,
-                            Lazy<ITicketTypeProvider> typeProvider,
-                           IMapper mapper)
+                            Func<TempDataDictionary,IGetsEditTicketModel> editTicketModelFactory,
+                            IMapper mapper)
     {
+      if(editTicketModelFactory == null)
+        throw new ArgumentNullException(nameof(editTicketModelFactory));
       if(mapper == null)
         throw new ArgumentNullException(nameof(mapper));
-      if(typeProvider == null)
-        throw new ArgumentNullException(nameof(typeProvider));
-      if(sprintLister == null)
-        throw new ArgumentNullException(nameof(sprintLister));
       if(editor == null)
         throw new ArgumentNullException(nameof(editor));
       if(ticketDetailService == null)
@@ -130,8 +99,7 @@ namespace Agiil.Web.Controllers
       this.mapper = mapper;
       this.ticketDetailService = ticketDetailService;
       this.editor = editor;
-      this.sprintLister = sprintLister;
-      this.typeProvider = typeProvider;
+      this.editTicketModelFactory = editTicketModelFactory;
     }
   }
 }
