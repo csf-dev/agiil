9ede13dbb67c2ceac686aa8a053557e6ff2d36ff
Craig Fowler <craig@csf-dev.com>
2018-08-08 21:47:16 +0100

    WIP #AG90 - Add a number of mappings

---
+++ Agiil.ObjectMaps/Tickets/AddRelationshipModelToAddRelationshipRequestProfile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Agiil.Domain.Tickets;
+using Agiil.Web.Models.Tickets;
+using AutoMapper;
+using CSF.Entities;
+
+namespace Agiil.ObjectMaps.Tickets
+{
+  public class AddRelationshipModelToAddRelationshipRequestProfile : Profile
+  {
+    public AddRelationshipModelToAddRelationshipRequestProfile()
+    {
+      CreateMap<AddRelationshipModel,AddRelationshipRequest>()
+        .ForMember(x => x.RelationshipId, opts => opts.ResolveUsing(GetRelationshipIdentity))
+        .ForMember(x => x.ParticipationType, opts => opts.ResolveUsing(GetRelationshipParticipant))
+        ;
+    }
+
+    IIdentity<Relationship> GetRelationshipIdentity(AddRelationshipModel model)
+    {
+      var values = GetIdAndParticipation(model?.RelationshipIdAndParticipation);
+      if(values == null) return null;
+
+      return Identity.Parse<Relationship>(values.Item1);
+    }
+
+    RelationshipParticipant GetRelationshipParticipant(AddRelationshipModel model)
+    {
+      var values = GetIdAndParticipation(model?.RelationshipIdAndParticipation);
+      if(values == null) return default(RelationshipParticipant);
+
+      return (RelationshipParticipant) Enum.Parse(typeof(RelationshipParticipant), values.Item2, true);
+    }
+
+    Tuple<string,string> GetIdAndParticipation(string combinedValue)
+    {
+      if(combinedValue == null) return null;
+      var parts = combinedValue.Split('-');
+      if(parts.Length != 2) return null;
+      return Tuple.Create(parts[0], parts[1]);
+    }
+  }
+}
--- Agiil.ObjectMaps/Tickets/TicketToTicketDetailDtoProfile.cs
+++ Agiil.ObjectMaps/Tickets/TicketToTicketDetailDtoProfile.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Agiil.Domain.Labels;
 using Agiil.Domain.Tickets;
 using Agiil.ObjectMaps.Resolvers;
 using Agiil.Web.Models.Tickets;
 using AutoMapper;
+using CSF.Entities;
 
 namespace Agiil.ObjectMaps.Tickets
 {
@@ -12,7 +14,8 @@ namespace Agiil.ObjectMaps.Tickets
   {
     public TicketToTicketDetailDtoProfile(TicketReferenceStringResolver ticketRefResolver,
                                           MarkdownToHtmlResolver markdownResolver,
-                                          IParsesLabelNames labelNameParser)
+                                          IParsesLabelNames labelNameParser,
+                                          IGetsRelationshipSummary summaryProvider)
     {
       CreateMap<Ticket,TicketDetailDto>()
         .ForMember(x => x.Id, o => o.ResolveUsing<IdentityValueResolver>())
@@ -23,10 +26,56 @@ namespace Agiil.ObjectMaps.Tickets
         .ForMember(x => x.TypeName, o => o.ResolveUsing(t => t.Type?.Name))
         .ForMember(x => x.CommaSeparatedLabelNames,
                    o => o.ResolveUsing(t => labelNameParser.GetCommaSeparatedLabelNames(t.Labels)))
-        .AfterMap((ticket, dto) => {
-          dto.Comments = dto.Comments.OrderBy(x => x.Timestamp).ToArray();
+        .AfterMap((ticket, dto, ctx) => {
+          dto.Comments = GetOrderedComments(dto);
+          dto.Relationships = GetRelationships(ticket, summaryProvider, ctx);
         })
         ;
+    }
+
+    IEnumerable<TicketRelationshipDto> GetRelationships(Ticket ticket,
+                                                        IGetsRelationshipSummary summaryProvider,
+                                                        ResolutionContext ctx)
+    {
+      return GetRelationships(ticket.PrimaryRelationships, RelationshipParticipant.Primary, summaryProvider, ctx)
+        .Union(GetRelationships(ticket.SecondaryRelationships, RelationshipParticipant.Secondary, summaryProvider, ctx));
+    }
+
+    IEnumerable<TicketRelationshipDto> GetRelationships(IEnumerable<TicketRelationship> relationships,
+                                                        RelationshipParticipant participant,
+                                                        IGetsRelationshipSummary summaryProvider,
+                                                        ResolutionContext ctx)
+    {
+      return relationships
+        .Where(x => x != null)
+        .Select(x => GetRelationship(x, participant, summaryProvider, ctx))
+        .ToArray();
+    }
+
+    TicketRelationshipDto GetRelationship(TicketRelationship relationship,
+                                          RelationshipParticipant participant,
+                                          IGetsRelationshipSummary summaryProvider,
+                                          ResolutionContext ctx)
+    {
+
+      Ticket relatedTicket;
+      if(participant == RelationshipParticipant.Primary)
+        relatedTicket = relationship.SecondaryTicket;
+      else
+        relatedTicket = relationship.PrimaryTicket;
+      
+      return new TicketRelationshipDto {
+        Id = relationship.GetIdentity(),
+        RelationshipId = relationship.Relationship?.GetIdentity(),
+        Summary = summaryProvider.GetSummary(relationship.Relationship, participant),
+        Participant = participant,
+        RelatedTicket = ctx.Mapper.Map<TicketSummaryDto>(relatedTicket),
+      };
+    }
+
+    IReadOnlyList<CommentDto> GetOrderedComments(TicketDetailDto dto)
+    {
+      return dto.Comments.OrderBy(x => x.Timestamp).ToArray();
     }
   }
 }
