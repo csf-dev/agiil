f30ea810ac731b1c0e6e2dbc580c1c5453aefc1d
Craig Fowler <craig@csf-dev.com>
2019-01-31 19:25:41 +0000

    Minor update to Mandatory function

--- Agiil.Web.Client/src/util/mandatory.js
+++ Agiil.Web.Client/src/util/mandatory.js
@@ -1,6 +1,6 @@
 //@flow
 
-export default function mandatory<T>(maybe : ?T) : T {
+export default function mandatory<T>(maybe : T | null | void) : T {
     if(maybe === null) throw new Error('`mandatory` requires an object but was passed a null reference');
     if(maybe === undefined) throw new Error('`mandatory` requires an object but was passed an undefined reference');
     return maybe;
