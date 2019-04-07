8ed524b42f190a778282b60d9a472897abb35c54
Craig Fowler <craig@csf-dev.com>
2019-04-07 11:29:18 +0100

    WIP #AG246 - Create log4net DI module

---
+++ Agiil.Domain.Impl/Log4NetLoggerForTypeProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using log4net;
+
+namespace Agiil.Domain
+{
+  public class Log4NetLoggerForTypeProvider : IGetsLoggerForType
+  {
+    public ILog GetLogger(Type type) => LogManager.GetLogger(type);
+  }
+}
