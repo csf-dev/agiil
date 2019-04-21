978c0976621f132fc4a7e4cf61ca879b0eb19bb7
Craig Fowler <craig@csf-dev.com>
2019-04-21 08:58:15 +0100

    Revert "Attempt to fix #294 by detecting an already-loaded page"
    This reverts commit 5f94a52250bbdb9e8e939af98b76b59e82fb1631.

--- Agiil.Web.Client/src/util/pageStarter.js
+++ Agiil.Web.Client/src/util/pageStarter.js
@@ -1,20 +1,7 @@
 //@flow
 
 function pageStarter(startup : () => void) {
-    let started = false;
-
-    // Event handler for when the page loads
-    document.addEventListener("DOMContentLoaded", () => {
-        if(started) return;
-        startup();
-        started = true;
-    });
-
-    // In case the page is already loaded
-    if(!started && document.readyState !== 'loading') {
-        startup();
-        started = true;
-    }
+    document.addEventListener("DOMContentLoaded", startup);
 }
 
 export default pageStarter;
