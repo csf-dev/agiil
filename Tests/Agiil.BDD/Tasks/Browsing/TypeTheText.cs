268b9f3bd21b73d6a73e1ebb1c1995371e64375f
Craig Fowler <craig@csf-dev.com>
2019-04-24 21:02:41 +0100

    WIP #AG297 - Attempt to fix problem by typing one character at a time
    I suspect that my script is overrunning the web browsers; typing
    characters too quickly for the browser to keep up.
    
    This is an attempt to fix that (if that's even the problem) by sending the
    key presses more slowly.

---
+++ Tests/Agiil.BDD/Tasks/Browsing/TypeTheText.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CSF.Screenplay.Actors;
+using CSF.Screenplay.Performables;
+using CSF.Screenplay.Selenium.Builders;
+using CSF.Screenplay.Selenium.Models;
+
+namespace Agiil.BDD.Tasks.Browsing
+{
+  public class TypeTheText : Performable
+  {
+    readonly string text;
+    readonly ITarget target;
+
+    protected override string GetReport(INamed actor)
+      => $"{actor.Name} types the text {text} into {target.GetName()}";
+
+    protected override void PerformAs(IPerformer actor)
+    {
+      var characters = text.ToCharArray();
+
+      for(var i = 0; i < characters.Length; i++)
+      {
+        var character = characters[i];
+
+        actor.Perform(Enter.TheText(character.ToString()).Into(target));
+
+        var isLast = (i == characters.Length - 1);
+        if(!isLast) actor.Perform(Wait.For(2).Milliseconds());
+      }
+    }
+
+    public TypeTheText(string text, ITarget target)
+    {
+      this.text = text ?? throw new ArgumentNullException(nameof(text));
+      this.target = target ?? throw new ArgumentNullException(nameof(target));
+    }
+  }
+}
---
+++ Tests/Agiil.BDD/Tasks/Type.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Agiil.BDD.Tasks.Browsing;
+using CSF.Screenplay.Performables;
+using CSF.Screenplay.Selenium.Models;
+
+namespace Agiil.BDD.Tasks
+{
+  public class Type
+  {
+    readonly string text;
+
+    public static Type TheText(string text) => new Type(text);
+
+    public IPerformable Into(ITarget target) => new TypeTheText(text, target);
+
+    Type(string text)
+    {
+      this.text = text;
+    }
+  }
+}
