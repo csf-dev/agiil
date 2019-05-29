5b5da37022be0f2b6115e6f45685009943690e0e
Craig Fowler <craig@csf-dev.com>
2019-05-29 19:01:50 +0100

    WIP #AG262 - Add beginnings of test coverage for scraping ticket detail

--- Agiil.Web.Client/src/models/tickets/TicketDetail.js
+++ Agiil.Web.Client/src/models/tickets/TicketDetail.js
@@ -11,6 +11,8 @@ export default class TicketDetail {
     descriptionMarkup : string;
     comments : Array<TicketComment>;
     newCommentFeedback : ?FeedbackMessage;
+    isOpen : bool;
+
 
     getTicketReference() : TicketReference {
         return { reference : this.reference };
