4edb48ad9551c67e49689723138f782aa66a98a5
Craig Fowler <craig@csf-dev.com>
2019-05-29 19:59:04 +0100

    WIP #AG262 - Add serialized ticket data to web page

---
+++ Agiil.Web/ActionFilters/ActionExecutedContextExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Agiil.Web.ActionFilters
+{
+  public static class ActionExecutedContextExtensions
+  {
+    public static T GetResult<T>(this ActionExecutedContext context) where T : ActionResult
+    {
+      if(context == null)
+        throw new ArgumentNullException(nameof(context));
+
+      return context.Result as T;
+    }
+  }
+}
---
+++ Agiil.Web/ActionFilters/ObjectSerializerPopulator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.Mvc;
+using Agiil.Web.Services;
+
+namespace Agiil.Web.ActionFilters
+{
+  public class ObjectSerializerPopulator : IActionFilter
+  {
+    readonly Func<ISerialisesToJson> serializerFactory;
+
+    public void OnActionExecuted(ActionExecutedContext filterContext)
+    {
+      var viewResult = filterContext.GetResult<ViewResult>();
+      if(viewResult == null) return;
+
+      viewResult.ViewBag.JsonSerializer = serializerFactory();
+    }
+
+    public void OnActionExecuting(ActionExecutingContext filterContext) { /* No-op */ }
+
+    public ObjectSerializerPopulator(Func<ISerialisesToJson> serializerFactory)
+    {
+      if(serializerFactory == null)
+        throw new ArgumentNullException(nameof(serializerFactory));
+
+      this.serializerFactory = serializerFactory;
+    }
+  }
+}
