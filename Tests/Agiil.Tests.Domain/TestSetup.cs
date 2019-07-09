3e8ac48755656b3e70bc2597951804c2a3f3f076
Craig Fowler <craig@csf-dev.com>
2019-07-09 19:40:59 +0100

    WIP #228 - Improve logging for test project

---
+++ Tests/Agiil.Tests.Domain/TestSetup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Agiil.Tests
+{
+  [SetUpFixture]
+  public class TestSetup
+  {
+    [OneTimeSetUp]
+    public void GlobalSetup()
+    {
+      log4net.Config.XmlConfigurator.Configure();
+    }
+  }
+}
