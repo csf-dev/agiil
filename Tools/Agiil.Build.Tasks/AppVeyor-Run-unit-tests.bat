3baffc4596c0195cd2caa41c7756ff98a56737e1
Craig Fowler <craig@csf-dev.com>
2019-07-21 11:47:48 +0100

    Attempt to fix AppVeyor build and tidy up

---
+++ Tools/Agiil.Build.Tasks/AppVeyor-Run-unit-tests.bat
@@ -0,0 +1,7 @@
+﻿%APPVEYOR_BUILD_FOLDER%\packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console.exe ^
+  --result=%APPVEYOR_BUILD_FOLDER%\Tests\Temp\TestResult.unit-tests.xml ^
+  --result=%APPVEYOR_BUILD_FOLDER%\Tests\Temp\TestResult.unit-tests.AppVeyor.xml;format=AppVeyor ^
+  Tests\Agiil.Tests.Auth\bin\Debug\Agiil.Tests.Auth.dll ^
+  Tests\Agiil.Tests.Data\bin\Debug\Agiil.Tests.Data.dll ^
+  Tests\Agiil.Tests.Domain\bin\Debug\Agiil.Tests.Services.dll ^
+  Tests\Agiil.Tests.Web\bin\Debug\Agiil.Tests.Web.dll
