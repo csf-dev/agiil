8829d4a7f2a4e341e2ea6c5089bcb27136627719
Craig Fowler <craig@csf-dev.com>
2019-07-21 14:34:11 +0100

    Improve code coverage in tests (to satisfy SonarQube)

---
+++ Tests/Agiil.Tests.Domain/Tickets/RelationshipValidation/TraversibleRelationshipTests.cs
@@ -0,0 +1,44 @@
+﻿using Agiil.Domain.Tickets;
+using Agiil.Domain.Tickets.RelationshipValidation;
+using Agiil.Tests.Attributes;
+using CSF.Entities;
+using NUnit.Framework;
+using System;
+namespace Agiil.Tests.Tickets.RelationshipValidation
+{
+  [TestFixture,Parallelizable]
+  public class TraversibleRelationshipTests
+  {
+    [Test, AutoMoqData]
+    public void Equals_returns_true_for_two_equal_identities([HasIdentity] TicketRelationship ticketRel,
+                                                             [HasIdentity] DirectionalRelationship rel1,
+                                                             [HasIdentity] DirectionalRelationship rel2)
+    {
+      var sut = new TraversibleRelationship(ticketRel.GetIdentity(), null, null, rel1.GetIdentity());
+      var two = new TraversibleRelationship(ticketRel.GetIdentity(), null, null, rel2.GetIdentity());
+
+      Assert.That(() => sut.Equals(two), Is.True);
+    }
+
+    [Test, AutoMoqData]
+    public void Equals_returns_false_for_two_different_identities([HasIdentity] TicketRelationship ticketRel1,
+                                                                  [HasIdentity] TicketRelationship ticketRel2,
+                                                                  [HasIdentity] DirectionalRelationship rel1,
+                                                                  [HasIdentity] DirectionalRelationship rel2)
+    {
+      var sut = new TraversibleRelationship(ticketRel1.GetIdentity(), null, null, rel1.GetIdentity());
+      var two = new TraversibleRelationship(ticketRel2.GetIdentity(), null, null, rel2.GetIdentity());
+
+      Assert.That(() => sut.Equals(two), Is.False);
+    }
+
+    [Test, AutoMoqData]
+    public void Equals_returns_false_for_object([HasIdentity] TicketRelationship ticketRel,
+                                                [HasIdentity] DirectionalRelationship rel1)
+    {
+      var sut = new TraversibleRelationship(ticketRel.GetIdentity(), null, null, rel1.GetIdentity());
+
+      Assert.That(() => sut.Equals(new Object()), Is.False);
+    }
+  }
+}
---
+++ Tests/Agiil.Tests.Web/Services/NewtonsoftJsonObjectSerialiserTests.cs
@@ -0,0 +1,29 @@
+﻿using Agiil.Tests.Attributes;
+using Agiil.Web.Services;
+using NUnit.Framework;
+using System;
+namespace Agiil.Tests.Web.Services
+{
+  [TestFixture,Parallelizable]
+  public class NewtonsoftJsonObjectSerialiserTests
+  {
+    [Test, AutoMoqData]
+    public void Serialize_returns_JSON_version_of_object(NewtonsoftJsonObjectSerialiser sut)
+    {
+      var obj = new TestObject {
+        StringProperty = "Hello",
+        NumericProperty = 22
+      };
+
+      var result = sut.Serialize(obj);
+
+      Assert.That(result, Is.EqualTo(@"{""StringProperty"":""Hello"",""NumericProperty"":22}"));
+    }
+  }
+
+  internal class TestObject
+  {
+    public string StringProperty { get; set; }
+    public int NumericProperty { get; set; }
+  }
+}
