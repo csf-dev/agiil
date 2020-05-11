608513b64312f685ed6255570bdd1c03c64e54a7
Craig Fowler <craig@csf-dev.com>
2020-05-11 21:46:49 +0100

    Fix related to #AG231 - Mapper resolution is OK to be transient

--- Agiil.Bootstrap/ObjectMaps/AutomapperModule.cs
+++ Agiil.Bootstrap/ObjectMaps/AutomapperModule.cs
@@ -12,9 +12,8 @@ namespace Agiil.Bootstrap.ObjectMaps
             builder
                 .Register(GetMapperConfiguration)
                 .SingleInstance();
-            builder
-                .Register(GetMapper)
-                .SingleInstance();
+
+            builder.Register(GetMapper);
         }
 
         MapperConfiguration GetMapperConfiguration(IComponentContext ctx)
@@ -24,9 +23,14 @@ namespace Agiil.Bootstrap.ObjectMaps
         {
             var config = ctx.Resolve<MapperConfiguration>();
             var scope = ctx.Resolve<ILifetimeScope>();
+
+            // Looking at https://github.com/AutoMapper/AutoMapper/blob/v6.0.2/src/AutoMapper/MapperConfiguration.cs#L250
+            // we can see that this is a trivially cheap operation, OK to happen as often as we need.
             var mapper = config.CreateMapper(scope.Resolve);
 
             return mapper;
         }
+
+
     }
 }
