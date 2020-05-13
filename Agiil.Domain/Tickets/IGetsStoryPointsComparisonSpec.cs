a9b22709c630c78b0dc2f3c33fbc498e22199236
Craig Fowler <craig@csf-dev.com>
2020-05-13 20:52:53 +0100

    WIP #AG249 - Add new specifications for story points

---
+++ Agiil.Domain/Tickets/IGetsStoryPointsComparisonSpec.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CSF.Specifications;
+
+namespace Agiil.Domain.Tickets
+{
+    public interface IGetsStoryPointsComparisonSpec
+    {
+        ISpecificationExpression<Ticket> Equals(int value);
+        ISpecificationExpression<Ticket> NotEquals(int value);
+
+        ISpecificationExpression<Ticket> GreaterThan(int value);
+        ISpecificationExpression<Ticket> GreaterThanOrEqual(int value);
+        ISpecificationExpression<Ticket> LessThan(int value);
+        ISpecificationExpression<Ticket> LessThanOrEqual(int value);
+
+        ISpecificationExpression<Ticket> GetFromPredicateName(string predicateName, int value);
+    }
+}
