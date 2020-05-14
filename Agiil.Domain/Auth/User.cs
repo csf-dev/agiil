39fe393536145da3a20b2c1cd9defdc0afeca223
Craig Fowler <craig@csf-dev.com>
2020-05-14 19:23:17 +0100

    Resolve #AG320 - Site administrators now have a db column
    At this point, there is no logic to actually make any users into
    site administrators.  This will have to be done manually in the db.

--- Agiil.Domain/Auth/User.cs
+++ Agiil.Domain/Auth/User.cs
@@ -4,10 +4,25 @@ using Agiil.Auth;
 
 namespace Agiil.Domain.Auth
 {
-  public class User : Entity<long>
-  {
-    public virtual string Username { get; set; }
+    public class User : Entity<long>
+    {
+        /// <summary>
+        /// The user's login username, such as 'joesmith'.
+        /// </summary>
+        /// <value>The username.</value>
+        public virtual string Username { get; set; }
 
-    public virtual string SerializedCredentials { get; set; }
-  }
+        /// <summary>
+        /// A serialized JSON object containing their credentials.
+        /// </summary>
+        /// <value>The serialized credentials.</value>
+        public virtual string SerializedCredentials { get; set; }
+
+        /// <summary>
+        /// A flag which indicates whether or not the user is a site-wide administrator user.
+        /// These super-admins have ultimate permissions to do anything in the whole app.
+        /// </summary>
+        /// <value><c>true</c> if this user is a site administrator; otherwise, <c>false</c>.</value>
+        public virtual bool SiteAdministrator { get; set; }
+    }
 }
