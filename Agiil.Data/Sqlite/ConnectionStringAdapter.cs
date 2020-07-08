d1adedc3dd82f9e484d5280c150c201d1e16cb64
Craig Fowler <craig@csf-dev.com>
2020-07-08 19:21:05 +0100

    Attempt to diagnose #AG351 by logging connection strings

--- Agiil.Data/Sqlite/ConnectionStringAdapter.cs
+++ Agiil.Data/Sqlite/ConnectionStringAdapter.cs
@@ -3,43 +3,36 @@ using System.Text.RegularExpressions;
 
 namespace Agiil.Data.Sqlite
 {
-  public class ConnectionStringAdapter
-  {
-    const string DataFilePattern = @"Data Source=([^;]+)";
-    static readonly Regex DataFileMatcher = new Regex(DataFilePattern,
-                                                      RegexOptions.CultureInvariant
-                                                      | RegexOptions.IgnoreCase
-                                                      | RegexOptions.Compiled);
-
-    readonly string connectionString;
-
-    public string ConnectionString => connectionString;
-
-    public string GetDataFile()
+    public class ConnectionStringAdapter
     {
-      var match = DataFileMatcher.Match(ConnectionString);
+        const string DataFilePattern = @"Data Source=([^;]+)";
+        static readonly Regex DataFileMatcher = new Regex(DataFilePattern,
+                                                          RegexOptions.CultureInvariant
+                                                          | RegexOptions.IgnoreCase
+                                                          | RegexOptions.Compiled);
 
-      if(!match.Success)
-        return null;
+        public string ConnectionString { get; }
 
-      return match.Groups[1].Value;
+        public string GetDataFile()
+        {
+            var match = DataFileMatcher.Match(ConnectionString);
+            Console.WriteLine(ConnectionString);
+
+            return match.Success ? match.Groups[1].Value : null;
+        }
+
+        public ConnectionStringAdapter(string connectionString)
+        {
+            ConnectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        }
+
+        public static ConnectionStringAdapter Create(IConnectionStringProvider connectionStringProvider)
+        {
+            if(connectionStringProvider == null)
+                throw new ArgumentNullException(nameof(connectionStringProvider));
+
+            var connString = connectionStringProvider.GetConnectionString();
+            return new ConnectionStringAdapter(connString);
+        }
     }
-
-    public ConnectionStringAdapter(string connectionString)
-    {
-      if(connectionString == null)
-        throw new ArgumentNullException(nameof(connectionString));
-
-      this.connectionString = connectionString;
-    }
-
-    public static ConnectionStringAdapter Create(IConnectionStringProvider connectionStringProvider)
-    {
-      if(connectionStringProvider == null)
-        throw new ArgumentNullException(nameof(connectionStringProvider));
-
-      var connString = connectionStringProvider.GetConnectionString();
-      return new ConnectionStringAdapter(connString);
-    }
-  }
 }
