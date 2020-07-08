2689c2150084aeb514403ffa7980c051e03dc5f5
Craig Fowler <craig@csf-dev.com>
2020-07-08 20:15:49 +0100

    WIP #AG351 - Attempt to diagnose problem with further console logging

--- Agiil.Bootstrap/Data/DataModule.cs
+++ Agiil.Bootstrap/Data/DataModule.cs
@@ -2,32 +2,28 @@
 using Agiil.Data;
 using Agiil.Domain.Data;
 using Autofac;
+using CSF.DecoratorBuilder;
 
 namespace Agiil.Bootstrap.Data
 {
-  public class DataModule : Module
-  {
-    protected override void Load(ContainerBuilder builder)
+    public class DataModule : Module
     {
-      base.Load(builder);
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
 
-      builder
-        .RegisterType<SnapshotStore>()
-        .SingleInstance();
+            builder
+              .RegisterType<SnapshotStore>()
+              .SingleInstance();
 
-      builder.Register(GetBackupTakingUpgrader);
-      
-      builder
-        .RegisterConfiguration<DataDirectoryConfigurationSection>()
-        .AsSelf()
-        .As<IGetsDataDirectory>();
+            builder.RegisterDecoratedService<IPerformsDatabaseUpgrades>(d => d
+                .UsingInitialImpl<DbUpDatabaseUpgrader>()
+                .ThenWrapWith<BackupTakingUpgrader>());
+
+            builder
+              .RegisterConfiguration<DataDirectoryConfigurationSection>()
+              .AsSelf()
+              .As<IGetsDataDirectory>();
+        }
     }
-
-    IPerformsDatabaseUpgrades GetBackupTakingUpgrader(IComponentContext c)
-    {
-      var factory = c.Resolve<Func<IPerformsDatabaseUpgrades,BackupTakingUpgrader>>();
-      var innerUpgrader = c.Resolve<DbUpDatabaseUpgrader>();
-      return factory(innerUpgrader);
-    }
-  }
 }
--- Agiil.Data/Sqlite/DatabaseDeleter.cs
+++ Agiil.Data/Sqlite/DatabaseDeleter.cs
@@ -3,15 +3,19 @@ using System.IO;
 
 namespace Agiil.Data.Sqlite
 {
-  public class DatabaseDeleter : IDeletesDatabase
-  {
-    readonly ConnectionStringAdapter connectionStringAdapter;
-
-    public void DeleteDatabase() => File.Delete(connectionStringAdapter.GetDataFile());
-
-    public DatabaseDeleter(IConnectionStringProvider connectionStringProvider)
+    public class DatabaseDeleter : IDeletesDatabase
     {
-      connectionStringAdapter = ConnectionStringAdapter.Create(connectionStringProvider);
+        readonly ConnectionStringAdapter connectionStringAdapter;
+
+        public void DeleteDatabase()
+        {
+            Console.WriteLine("Deleting the database ...");
+            File.Delete(connectionStringAdapter.GetDataFile());
+        }
+
+        public DatabaseDeleter(IConnectionStringProvider connectionStringProvider)
+        {
+            connectionStringAdapter = ConnectionStringAdapter.Create(connectionStringProvider);
+        }
     }
-  }
 }
