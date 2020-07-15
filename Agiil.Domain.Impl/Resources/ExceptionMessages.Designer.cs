cbc6dac968b965492b1145129da955e257060da3
Craig Fowler <craig@csf-dev.com>
2020-07-15 19:56:59 +0100

    WIP #AG352 - Fix resource files

--- Agiil.Domain.Impl/Resources/ExceptionMessages.Designer.cs
+++ Agiil.Domain.Impl/Resources/ExceptionMessages.Designer.cs
@@ -30,7 +30,7 @@ namespace Agiil.Domain.Resources {
         internal static System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.Equals(null, resourceMan)) {
-                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("Agiil.Domain.ExceptionMessages.resources", typeof(ExceptionMessages).Assembly);
+                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("Agiil.Domain.Resources.ExceptionMessages", typeof(ExceptionMessages).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
