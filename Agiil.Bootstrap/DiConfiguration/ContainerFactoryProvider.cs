b03c0fe86bab2119c4ad84d5b98d6ac498ac9cad
Craig Fowler <craig@csf-dev.com>
2020-07-15 20:30:25 +0100

    WIP #AG352 - Attempt to diagnose/fix Travis build failure: Add error handling
    Now, the DI container factory will raise an exception if
    the specified type is not found, and that exception will
    have appropriate info about what's wrong.

--- Agiil.Bootstrap/DiConfiguration/ContainerFactoryProvider.cs
+++ Agiil.Bootstrap/DiConfiguration/ContainerFactoryProvider.cs
@@ -3,37 +3,74 @@ using CSF.Configuration;
 
 namespace Agiil.Bootstrap.DiConfiguration
 {
-  public class ContainerFactoryProvider
-  {
-    public IGetsAutofacContainer GetContainerBuilderFactory()
+    public class ContainerFactoryProvider
     {
-      var factoryType = GetFactoryType();
+        readonly IConfigurationReader configReader;
 
-      return GetFactory(factoryType);
+        public IGetsAutofacContainer GetContainerBuilderFactory()
+        {
+            var factoryType = GetFactoryType();
+
+            return (IGetsAutofacContainer) Activator.CreateInstance(factoryType);
+        }
+
+        Type GetFactoryType()
+        {
+            var config = configReader.ReadSection<DiConfigurationSection>();
+
+            if(String.IsNullOrEmpty(config?.FactoryTypeName))
+                return typeof(DomainContainerFactory);
+
+            var type = Type.GetType(config.FactoryTypeName);
+            if(type == null)
+                throw new CannotGetDiContainerFactoryException($"Configuration specifies container factory type `{config.FactoryTypeName}' but this type cannot be found.");
+
+            return type;
+        }
+
+        public ContainerFactoryProvider(IConfigurationReader configReader)
+        {
+            this.configReader = configReader ?? throw new ArgumentNullException(nameof(configReader));
+        }
+
+        /// <summary>
+        /// Exception raised when a DI container factory cannot be created.
+        /// </summary>
+        [Serializable]
+        public class CannotGetDiContainerFactoryException : Exception
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="CannotGetDiContainerFactoryException"/> class
+            /// </summary>
+            public CannotGetDiContainerFactoryException()
+            {
+            }
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="CannotGetDiContainerFactoryException"/> class
+            /// </summary>
+            /// <param name="message">A <see cref="System.String"/> that describes the exception. </param>
+            public CannotGetDiContainerFactoryException(string message) : base(message)
+            {
+            }
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="CannotGetDiContainerFactoryException"/> class
+            /// </summary>
+            /// <param name="message">A <see cref="System.String"/> that describes the exception. </param>
+            /// <param name="inner">The exception that is the cause of the current exception. </param>
+            public CannotGetDiContainerFactoryException(string message, Exception inner) : base(message, inner)
+            {
+            }
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="CannotGetDiContainerFactoryException"/> class
+            /// </summary>
+            /// <param name="context">The contextual information about the source or destination.</param>
+            /// <param name="info">The object that holds the serialized object data.</param>
+            protected CannotGetDiContainerFactoryException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context)
+            {
+            }
+        }
     }
-
-    Type GetFactoryType()
-    {
-      var config = GetConfig();
-
-      if(config != null)
-        return Type.GetType(config.FactoryTypeName);
-
-      return typeof(DomainContainerFactory);
-    }
-
-    DiConfigurationSection GetConfig()
-    {
-      var reader = new ConfigurationReader();
-      return reader.ReadSection<DiConfigurationSection>();
-    }
-
-    IGetsAutofacContainer GetFactory(Type factoryType)
-    {
-      if(factoryType == null)
-        throw new ArgumentNullException(nameof(factoryType));
-
-      return (IGetsAutofacContainer) Activator.CreateInstance(factoryType);
-    }
-  }
 }
