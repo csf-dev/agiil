65a80b4532bd4df9d6d1bb4b7a5cbcd0c8823963
Craig Fowler <craig@csf-dev.com>
2020-07-17 20:10:27 +0100

    Trivial - whitespace change

--- Agiil.Domain.Impl/TicketCriterionConvertionStrategies/TicketIsOpenClosedConversionStrategy.cs
+++ Agiil.Domain.Impl/TicketCriterionConvertionStrategies/TicketIsOpenClosedConversionStrategy.cs
@@ -6,41 +6,41 @@ using CSF.Specifications;
 
 namespace Agiil.Domain.TicketCriterionConvertionStrategies
 {
-  public class TicketIsOpenClosedConversionStrategy : IStrategyForConvertingCriterionToSpecification
-  {
-    readonly IResolvesValue valueResolver;
-
-    public ISpecificationExpression<Ticket> ConvertToSpecification(Criterion criterion)
+    public class TicketIsOpenClosedConversionStrategy : IStrategyForConvertingCriterionToSpecification
     {
-      var predicateAndValue = criterion?.Test as PredicateAndValue;
-      if(predicateAndValue == null) return null;
-      var value = valueResolver.Resolve<string>(predicateAndValue.Value);
+        readonly IResolvesValue valueResolver;
 
-      if(value == WellKnownValue.Closed)
-        return new IsClosed();
+        public ISpecificationExpression<Ticket> ConvertToSpecification(Criterion criterion)
+        {
+            var predicateAndValue = criterion?.Test as PredicateAndValue;
+            if(predicateAndValue == null) return null;
+            var value = valueResolver.Resolve<string>(predicateAndValue.Value);
 
-      return new IsOpen();
+            if(value == WellKnownValue.Closed)
+                return new IsClosed();
+
+            return new IsOpen();
+        }
+
+        public static CriterionToSpecificationConversionStrategyMetadata GetMetadata() => new TicketIsOpenClosedConversionMetadata();
+
+        CriterionToSpecificationConversionStrategyMetadata IStrategyForConvertingCriterionToSpecification.GetMetadata()
+                                                                                                         => GetMetadata();
+
+        public TicketIsOpenClosedConversionStrategy(IResolvesValue valueResolver)
+        {
+            if(valueResolver == null)
+                throw new ArgumentNullException(nameof(valueResolver));
+            this.valueResolver = valueResolver;
+        }
+
+        class TicketIsOpenClosedConversionMetadata : CriterionToSpecificationConversionStrategyMetadata
+        {
+            public override bool CanConvertAsPredicateWithValue(string elementName, string predicateText)
+            {
+                return (elementName == ElementName.Ticket
+                        && predicateText == PredicateName.Is);
+            }
+        }
     }
-
-    public static CriterionToSpecificationConversionStrategyMetadata GetMetadata() => new TicketIsOpenClosedConversionMetadata();
-
-    CriterionToSpecificationConversionStrategyMetadata IStrategyForConvertingCriterionToSpecification.GetMetadata()
-                                                                                                     => GetMetadata();
-
-    public TicketIsOpenClosedConversionStrategy(IResolvesValue valueResolver)
-    {
-      if(valueResolver == null)
-        throw new ArgumentNullException(nameof(valueResolver));
-      this.valueResolver = valueResolver;
-    }
-
-    class TicketIsOpenClosedConversionMetadata : CriterionToSpecificationConversionStrategyMetadata
-    {
-			public override bool CanConvertAsPredicateWithValue(string elementName, string predicateText)
-			{
-        return (elementName == ElementName.Ticket
-                && predicateText == PredicateName.Is);
-			}
-    }
-  }
 }
