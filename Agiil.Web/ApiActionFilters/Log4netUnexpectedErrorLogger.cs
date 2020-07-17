c5b67673a4a99671a18e96350e904763108d6177
Craig Fowler <craig@csf-dev.com>
2020-07-17 19:13:03 +0100

    WIP #AG352 - Improve global error logging for API usage
    Previously, some errors such as DI errors weren't being logged.

---
+++ Agiil.Web/ApiActionFilters/Log4netUnexpectedErrorLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Http.ExceptionHandling;
+using log4net;
+
+namespace Agiil.Web.ApiActionFilters
+{
+    public class Log4netUnexpectedErrorLogger : ExceptionLogger
+    {
+        readonly static ILog logger;
+
+        public override void Log(ExceptionLoggerContext context)
+        {
+            logger.Error($@"Unhandled exception at {context.Request.RequestUri}
+{context.Exception}");
+        }
+
+        static Log4netUnexpectedErrorLogger()
+        {
+            logger = LogManager.GetLogger(typeof(Log4netUnexpectedErrorLogger));
+        }
+    }
+}
