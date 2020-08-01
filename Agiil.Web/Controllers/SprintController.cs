b453f76e316bea25675d4b5919271f3c697e916f
Craig Fowler <craig@csf-dev.com>
2020-08-01 15:02:30 +0100

    WIP #AG30 - Convert items marked TODO to use AutoMapper
    A number of TODO comments had been left relating to this ticket,
    these are all now converted to use Automapper.

--- Agiil.Web/Controllers/SprintController.cs
+++ Agiil.Web/Controllers/SprintController.cs
@@ -10,85 +10,80 @@ using CSF.Entities;
 
 namespace Agiil.Web.Controllers
 {
-  public class SprintController : Controller
-  {
-    const string
-      EditSprintModelKey = "Edit sprint model",
-      SuccessfulEditKey = "Successful edit";
-
-
-    readonly Lazy<ISprintDetailService> detailService;
-    readonly Lazy<ISprintEditor> editor;
-    readonly IMapper mapper;
-
-    public ActionResult Index(IIdentity<Sprint> id)
+    public class SprintController : Controller
     {
-      var sprint = detailService.Value.GetSprint(id);
+        const string
+          EditSprintModelKey = "Edit sprint model",
+          SuccessfulEditKey = "Successful edit";
 
-      if(ReferenceEquals(sprint, null))
-        return HttpNotFound();
 
-      var model = new ViewSprintModel();
-      model.Sprint = mapper.Map<SprintDetailDto>(sprint);
-      return View(model);
-    }
+        readonly Lazy<ISprintDetailService> detailService;
+        readonly Lazy<ISprintEditor> editor;
+        readonly IMapper mapper;
 
-    [HttpGet]
-    public ActionResult Edit(IIdentity<Sprint> id)
-    {
-      var sprint = detailService.Value.GetSprint(id);
+        public ActionResult Index(IIdentity<Sprint> id)
+        {
+            var sprint = detailService.Value.GetSprint(id);
 
-      if(ReferenceEquals(sprint, null))
-        return HttpNotFound();
+            if(ReferenceEquals(sprint, null))
+                return HttpNotFound();
 
-      var response = TempData.TryGet<Models.Sprints.EditSprintResponse>(EditSprintModelKey);
-      var model = CreateModel();
-      model.Response = response;
-      model.Sprint = mapper.Map<SprintDetailDto>(sprint);
-      return View(model);
-    }
+            var model = new ViewSprintModel();
+            model.Sprint = mapper.Map<SprintDetailDto>(sprint);
+            return View(model);
+        }
 
-    [HttpPost]
-    public ActionResult Edit(EditSprintSpecification spec)
-    {
-      var request = mapper.Map<EditSprintRequest>(spec);
-      var response = editor.Value.Edit(request);
+        [HttpGet]
+        public ActionResult Edit(IIdentity<Sprint> id)
+        {
+            var sprint = detailService.Value.GetSprint(id);
 
-      if(response.IdIsInvalid)
-        return HttpNotFound();
+            if(ReferenceEquals(sprint, null))
+                return HttpNotFound();
 
-      TempData.Clear();
+            var response = TempData.TryGet<Models.Sprints.EditSprintResponse>(EditSprintModelKey);
+            var model = new EditSprintModel();
+            model.Response = response;
+            model.Sprint = mapper.Map<SprintDetailDto>(sprint);
+            return View(model);
+        }
 
-      if(response.IsSuccess)
-      {
-        TempData.Add(SuccessfulEditKey, true);
-        return RedirectToAction(nameof(Index), new { id = spec.Id });
-      }
+        [HttpPost]
+        public ActionResult Edit(EditSprintSpecification spec)
+        {
+            var request = mapper.Map<EditSprintRequest>(spec);
+            var response = editor.Value.Edit(request);
 
-      var responseModel = mapper.Map<Models.Sprints.EditSprintResponse>(response);
-      TempData.Add(EditSprintModelKey, responseModel);
+            if(response.IdIsInvalid)
+                return HttpNotFound();
 
-      return RedirectToAction(nameof(Edit), new { id = spec.Id });
-    }
+            TempData.Clear();
 
-    EditSprintModel CreateModel()
-    {
-      return new EditSprintModel();
-    }
+            if(response.IsSuccess)
+            {
+                TempData.Add(SuccessfulEditKey, true);
+                return RedirectToAction(nameof(Index), new { id = spec.Id });
+            }
 
-    public SprintController(Lazy<ISprintDetailService> detailService,
+            var responseModel = mapper.Map<Models.Sprints.EditSprintResponse>(response);
+            TempData.Add(EditSprintModelKey, responseModel);
+
+            return RedirectToAction(nameof(Edit), new { id = spec.Id });
+        }
+
+        public SprintController(Lazy<ISprintDetailService> detailService,
                             Lazy<ISprintEditor> editor,
                             IMapper mapper)
-    {
-      if(mapper == null)
-        throw new ArgumentNullException(nameof(mapper));
-      if(editor == null)
-        throw new ArgumentNullException(nameof(editor));
-      if(detailService == null)
-        throw new ArgumentNullException(nameof(detailService));
-      this.mapper = mapper;
-      this.detailService = detailService;
-      this.editor = editor;
+        {
+            if(mapper == null)
+                throw new ArgumentNullException(nameof(mapper));
+            if(editor == null)
+                throw new ArgumentNullException(nameof(editor));
+            if(detailService == null)
+                throw new ArgumentNullException(nameof(detailService));
+            this.mapper = mapper;
+            this.detailService = detailService;
+            this.editor = editor;
+        }
     }
-  }
 }
