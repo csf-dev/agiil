14bca1d3976cbec05c28098571bc8bc1771193d7
Craig Fowler <craig@csf-dev.com>
2020-08-01 16:11:06 +0100

    Resolve #AG212 for now - minor refactoring of interface
    Actually, I have decided that I will not refactor this
    method, because it has been taken from elsewhere.  The chances
    that it will need maintenance are quite small.
    
    I have lowered its priority on SonarCloud as a result.

--- Agiil.Web.Rendering.Impl/MarkdownExtensions/AutoTicketLinkParser.cs
+++ Agiil.Web.Rendering.Impl/MarkdownExtensions/AutoTicketLinkParser.cs
@@ -9,173 +9,166 @@ using Markdig.Syntax.Inlines;
 
 namespace Agiil.Web.Rendering.MarkdownExtensions
 {
-  public class AutoTicketLinkParser : InlineParser
-  {
-    const string TicketLinkClass = "ticket_link";
-
-    readonly TicketReferenceParser referenceParser;
-    readonly Lazy<IGetsTicketUri> ticketUriProvider;
-
-    public override bool Match(InlineProcessor processor, ref StringSlice slice)
+    public class AutoTicketLinkParser : InlineParser
     {
-      var previousChar = slice.PeekCharExtra(-1);
-      if(!IsValidPreviousCharacter(previousChar)) return false;
+        const string TicketLinkClass = "ticket_link";
 
-      List<char> pendingEmphasis;
-      if (!IsAutoLinkValidInCurrentContext(processor, out pendingEmphasis)) return false;
+        readonly IParsesTicketReferenceFromMarkdigCharIterator referenceParser;
+        readonly Lazy<IGetsTicketUri> ticketUriProvider;
 
-      int charactersConsumedInReference;
-      var ticketUri = GetTicketUri(slice, out charactersConsumedInReference);
-      if(ticketUri == null) return false;
-
-      AddTicketLink(processor, slice, charactersConsumedInReference, ticketUri);
-      slice.Start += charactersConsumedInReference;
-
-      return true;
-    }
-
-    static void AddTicketLink(InlineProcessor processor,
-                              StringSlice slice,
-                              int charactersConsumedInReference,
-                              Uri uri)
-    {
-      int line, column;
-
-      var startPosition = slice.Start;
-      var endPosition = startPosition + charactersConsumedInReference - 1;
-      var linkText = new StringSlice(slice.Text, startPosition, endPosition);
-
-      var link = new TicketLinkInline
-      {
-        Span =
+        public override bool Match(InlineProcessor processor, ref StringSlice slice)
         {
-          Start = processor.GetSourcePosition(startPosition, out line, out column),
-        },
-        Line = line,
-        Column = column,
-        Url = uri.ToString(),
-        IsClosed = true,
-        IsAutoLink = true,
-        Title = $"Navigate to {linkText.ToString()}",
-      };
+            var previousChar = slice.PeekCharExtra(-1);
+            if(!IsValidPreviousCharacter(previousChar)) return false;
 
-      link.Span.End = endPosition;
-      link.UrlSpan = link.Span;
-      link.GetAttributes().AddClass(TicketLinkClass);
+            List<char> pendingEmphasis;
+            if(!IsAutoLinkValidInCurrentContext(processor, out pendingEmphasis)) return false;
 
-      var linkContent = new LiteralInline
-      {
-        Span = link.Span,
-        Line = line,
-        Column = column,
-        Content = linkText,
-        IsClosed = true,
-      };
+            int charactersConsumedInReference;
+            var ticketUri = GetTicketUri(slice, out charactersConsumedInReference);
+            if(ticketUri == null) return false;
 
-      link.AppendChild(linkContent);
+            AddTicketLink(processor, slice, charactersConsumedInReference, ticketUri);
+            slice.Start += charactersConsumedInReference;
 
-      processor.Inline = link;
-    }
-
-    Uri GetTicketUri(ICharIterator iterator, out int charactersConsumedInReference)
-    {
-      var ticketReference = referenceParser.GetTicketReference(iterator, out charactersConsumedInReference);
-      if(ticketReference == null) return null;
-
-      var ticketUri = ticketUriProvider.Value.GetAbsoluteUri(ticketReference);
-      return ticketUri;
-    }
-
-    static bool IsValidPreviousCharacter(char c)
-    {
-      // An auto-ticket link can only come at the beginning of a line, after
-      // whitespace, or any of the delimiting characters *, _, ~, and (.
-      return c.IsWhiteSpaceOrZero() || c == '*' || c == '_' || c == '~' || c == '(';
-    }
-
-    // This entire method is taken from the following code
-    // https://github.com/lunet-io/markdig/blob/0f7e3b8c5278442f2ec5965a8f2b2c9ee573227a/src/Markdig/Extensions/AutoLinks/AutoLinkParser.cs
-    // The conditions on which a ticket link would be valid are the same as an auto-link.
-    bool IsAutoLinkValidInCurrentContext(InlineProcessor processor, out List<char> pendingEmphasis)
-    {
-      pendingEmphasis = null;
-
-      // Case where there is a pending HtmlInline <a>
-      var currentInline = processor.Inline;
-      while (currentInline != null)
-      {
-        var htmlInline = currentInline as HtmlInline;
-        if (htmlInline != null)
-        {
-          // If we have a </a> we don't expect nested <a>
-          if (htmlInline.Tag.StartsWith("</a", StringComparison.OrdinalIgnoreCase))
-          {
-            break;
-          }
-
-          // If there is a pending <a>, we can't allow a link
-          if (htmlInline.Tag.StartsWith("<a", StringComparison.OrdinalIgnoreCase))
-          {
-            return false;
-          }
+            return true;
         }
 
-        // Check previous sibling and parents in the tree 
-        currentInline = currentInline.PreviousSibling ?? currentInline.Parent;
-      }
+        static void AddTicketLink(InlineProcessor processor,
+                                  StringSlice slice,
+                                  int charactersConsumedInReference,
+                                  Uri uri)
+        {
+            int line, column;
 
-      // Check that we don't have any pending brackets opened (where we could have a possible markdown link)
-      // NOTE: This assume that [ and ] are used for links, otherwise autolink will not work properly
-      currentInline = processor.Inline;
-      int countBrackets = 0;
-      while (currentInline != null)
-      {
-        var linkDelimiterInline = currentInline as LinkDelimiterInline;
-        if (linkDelimiterInline != null && linkDelimiterInline.IsActive)
-        {
-          if (linkDelimiterInline.Type == DelimiterType.Open)
-          {
-            countBrackets++;
-          }
-          else if (linkDelimiterInline.Type == DelimiterType.Close)
-          {
-            countBrackets--;
-          }
+            var startPosition = slice.Start;
+            var endPosition = startPosition + charactersConsumedInReference - 1;
+            var linkText = new StringSlice(slice.Text, startPosition, endPosition);
+
+            var link = new TicketLinkInline {
+                Span = {
+                  Start = processor.GetSourcePosition(startPosition, out line, out column),
+                },
+                Line = line,
+                Column = column,
+                Url = uri.ToString(),
+                IsClosed = true,
+                IsAutoLink = true,
+                Title = $"Navigate to {linkText.ToString()}",
+            };
+
+            link.Span.End = endPosition;
+            link.UrlSpan = link.Span;
+            link.GetAttributes().AddClass(TicketLinkClass);
+
+            var linkContent = new LiteralInline {
+                Span = link.Span,
+                Line = line,
+                Column = column,
+                Content = linkText,
+                IsClosed = true,
+            };
+
+            link.AppendChild(linkContent);
+
+            processor.Inline = link;
         }
-        else
+
+        Uri GetTicketUri(ICharIterator iterator, out int charactersConsumedInReference)
         {
-          // Record all pending characters for emphasis
-          var emphasisDelimiter = currentInline as EmphasisDelimiterInline;
-          if (emphasisDelimiter != null)
-          {
-            if (pendingEmphasis == null)
+            var ticketReference = referenceParser.GetTicketReference(iterator, out charactersConsumedInReference);
+            if(ticketReference == null) return null;
+
+            var ticketUri = ticketUriProvider.Value.GetAbsoluteUri(ticketReference);
+            return ticketUri;
+        }
+
+        static bool IsValidPreviousCharacter(char c)
+        {
+            // An auto-ticket link can only come at the beginning of a line, after
+            // whitespace, or any of the delimiting characters *, _, ~, and (.
+            return c.IsWhiteSpaceOrZero() || c == '*' || c == '_' || c == '~' || c == '(';
+        }
+
+        // This entire method is taken from the following code
+        // https://github.com/lunet-io/markdig/blob/0f7e3b8c5278442f2ec5965a8f2b2c9ee573227a/src/Markdig/Extensions/AutoLinks/AutoLinkParser.cs
+        // The conditions on which a ticket link would be valid are the same as an auto-link.
+        bool IsAutoLinkValidInCurrentContext(InlineProcessor processor, out List<char> pendingEmphasis)
+        {
+            pendingEmphasis = null;
+
+            // Case where there is a pending HtmlInline <a>
+            var currentInline = processor.Inline;
+            while(currentInline != null)
             {
-              // Not optimized for GC, but we don't expect this case much
-              pendingEmphasis = new List<char>();
+                var htmlInline = currentInline as HtmlInline;
+                if(htmlInline != null)
+                {
+                    // If we have a </a> we don't expect nested <a>
+                    if(htmlInline.Tag.StartsWith("</a", StringComparison.OrdinalIgnoreCase))
+                    {
+                        break;
+                    }
+
+                    // If there is a pending <a>, we can't allow a link
+                    if(htmlInline.Tag.StartsWith("<a", StringComparison.OrdinalIgnoreCase))
+                    {
+                        return false;
+                    }
+                }
+
+                // Check previous sibling and parents in the tree 
+                currentInline = currentInline.PreviousSibling ?? currentInline.Parent;
             }
-            if (!pendingEmphasis.Contains(emphasisDelimiter.DelimiterChar))
+
+            // Check that we don't have any pending brackets opened (where we could have a possible markdown link)
+            // NOTE: This assume that [ and ] are used for links, otherwise autolink will not work properly
+            currentInline = processor.Inline;
+            int countBrackets = 0;
+            while(currentInline != null)
             {
-              pendingEmphasis.Add(emphasisDelimiter.DelimiterChar);
+                var linkDelimiterInline = currentInline as LinkDelimiterInline;
+                if(linkDelimiterInline != null && linkDelimiterInline.IsActive)
+                {
+                    if(linkDelimiterInline.Type == DelimiterType.Open)
+                    {
+                        countBrackets++;
+                    }
+                    else if(linkDelimiterInline.Type == DelimiterType.Close)
+                    {
+                        countBrackets--;
+                    }
+                }
+                else
+                {
+                    // Record all pending characters for emphasis
+                    var emphasisDelimiter = currentInline as EmphasisDelimiterInline;
+                    if(emphasisDelimiter != null)
+                    {
+                        if(pendingEmphasis == null)
+                        {
+                            // Not optimized for GC, but we don't expect this case much
+                            pendingEmphasis = new List<char>();
+                        }
+                        if(!pendingEmphasis.Contains(emphasisDelimiter.DelimiterChar))
+                        {
+                            pendingEmphasis.Add(emphasisDelimiter.DelimiterChar);
+                        }
+                    }
+                }
+                currentInline = currentInline.Parent;
             }
-          }
+
+            return countBrackets <= 0;
         }
-        currentInline = currentInline.Parent;
-      }
 
-      return countBrackets <= 0;
+        public AutoTicketLinkParser(IParsesTicketReferenceFromMarkdigCharIterator referenceParser,
+                                    Lazy<IGetsTicketUri> ticketUriProvider)
+        {
+            this.referenceParser = referenceParser ?? throw new ArgumentNullException(nameof(referenceParser));
+            this.ticketUriProvider = ticketUriProvider ?? throw new ArgumentNullException(nameof(ticketUriProvider));
+
+            OpeningCharacters = new[] { '#' };
+        }
     }
-
-    public AutoTicketLinkParser(TicketReferenceParser referenceParser, Lazy<IGetsTicketUri> ticketUriProvider)
-    {
-      if(ticketUriProvider == null)
-        throw new ArgumentNullException(nameof(ticketUriProvider));
-      if(referenceParser == null)
-        throw new ArgumentNullException(nameof(referenceParser));
-      
-      this.referenceParser = referenceParser;
-      this.ticketUriProvider = ticketUriProvider;
-
-      OpeningCharacters = new [] { '#' };
-    }
-  }
 }
---
+++ Agiil.Web.Rendering.Impl/MarkdownExtensions/IParsesTicketReferenceFromMarkdigCharIterator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Agiil.Domain.Tickets;
+using Markdig.Helpers;
+
+namespace Agiil.Web.Rendering.MarkdownExtensions
+{
+    public interface IParsesTicketReferenceFromMarkdigCharIterator
+    {
+        TicketReference GetTicketReference(ICharIterator iterator, out int charactersConsumed);
+    }
+}
