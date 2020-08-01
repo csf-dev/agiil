468d585abcef53ab898fc1fcf8b90d99047d9ccc
Craig Fowler <craig@csf-dev.com>
2020-08-01 15:30:52 +0100

    Trivial - rename a file for clarity

---
+++ Tests/Agiil.Tests.Domain/GlobalOneTimeSetupForAllTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Agiil.Tests
+{
+    [SetUpFixture]
+    public class GlobalOneTimeSetupForAllTests
+    {
+        [OneTimeSetUp]
+        public void GlobalSetup()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+        }
+    }
+}
